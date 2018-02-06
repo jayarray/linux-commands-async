0b7529ba201718e461eb15675826039b89c9f2b8
Jesus Isaias Arevalo <isa90201@gmail.com>
2018-02-06 13:12:42 -0800

    Moved copy class to its own file.

---
+++ copy.js
@@ -0,0 +1,44 @@
+let PATH = require('./path.js');
+let FS = require('fs-extra');
+
+//-------------------------------------------------
+// COPY (cp)
+class Copy {
+  static copy(src, dest) {
+    return new Promise((resolve, reject) => {
+      let error = PATH.Error.PathError(src);
+      if (error) {
+        reject(`Source error: ${error}`);
+        return;
+      }
+
+      let error = PATH.Error.PathError(dest);
+      if (error) {
+        reject(`Destination error: ${error}`);
+        return;
+      }
+
+      PATH.Path.exists(src).then(exists => {
+        if (!exists) {
+          reject(`Source error: Path does not exist: ${src}`);
+          return;
+        }
+
+        FS.copy(src, dest, (err) => {
+          if (err) {
+            reject(`Copy failed: ${err}`);
+            return;
+          }
+          resolve(true);
+        });
+      }).catch(error => {
+        reject(`Source error: ${error}`);
+      });
+    });
+  }
+}
+
+//----------------------------------------
+// EXPORTS
+
+exports.Copy = Copy;
