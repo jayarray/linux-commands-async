9e7f68de7e981b8bb8a91a211c8a9c9a8a63f530
Jesus Isaias Arevalo <isa90201@gmail.com>
2018-02-06 14:27:50 -0800

    Moved Remove class into its own file.

---
+++ remove.js
@@ -0,0 +1,76 @@
+let PATH = require('./path.js');
+let FS = require('fs-extra');
+
+//-------------------------------------------------
+// REMOVE (rm)
+
+class Remove {
+  static File(path) {
+    return new Promise((resolve, reject) => {
+      let error = PATH.Error.PathError(path);
+      if (error) {
+        reject(error);
+        return;
+      }
+
+      PATH.Path.Exists(path).then(exists => {
+        if (!exists) {
+          reject(`Path does not exist: ${path}`);
+          return;
+        }
+
+        PATH.Path.IsFile(path).then(isFile => {
+          if (!isFile) {
+            resolve(`Path is not a file: ${path}`);
+            return;
+          }
+
+          FS.unlink(path, (err) => {
+            if (err) {
+              reject(`Failed to remove file: ${err}`);
+              return;
+            }
+            resolve(true);
+          });
+        }).catch(reject);
+      }).catch(reject);
+    });
+  }
+
+  static Directory(path) {
+    return new Promise((resolve, reject) => {
+      let error = PATH.Error.PathError(path);
+      if (error) {
+        reject(error);
+        return;
+      }
+
+      PATH.Path.Exists(path).then(exists => {
+        if (!exists) {
+          reject(`Path does not exist: ${path}`);
+          return;
+        }
+
+        Path.IsDir(path).then(isDir => {
+          if (!isDir) {
+            resolve(`Path is not a directory: ${path}`);
+            return;
+          }
+
+          RIMRAF(pTrimmed, (err) => {
+            if (err) {
+              reject(`Failed to remove directory: ${err}`);
+              return;
+            }
+            resolve(true);
+          });
+        }).catch(reject);
+      }).catch(reject);
+    });
+  }
+}
+
+//------------------------------
+// EXPORTS
+
+exports.Remove = Remove;
