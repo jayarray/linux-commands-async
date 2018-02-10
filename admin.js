8d29d817b45c593e5652cba80eb275b8b82b7151
Jesus Isaias Arevalo <isa90201@gmail.com>
2018-02-09 16:26:23 -0800

    fixed bugs. Moved raw command code blocks to their own Commands class.

--- admin.js
+++ admin.js
@@ -38,8 +38,10 @@ class Groups {
   static Exists(gid) {
     return new Promise((resolve, reject) => {
       let error = Error.IdError(gid);
-      if (error)
-        return `gid ${error}`;
+      if (error) {
+        reject(`gid ${error}`);
+        return;
+      }
 
       Groups.All().then(groups => {
         for (let i = 0; i < groups.length; ++i) {
@@ -52,6 +54,10 @@ class Groups {
         resolve(false);
       }).catch(reject);
     });
+  }
+
+  static Get(id) {
+    // Return group obj
   }
 }
 
@@ -90,8 +96,10 @@ class Users {
   static Exists(uid) {
     return new Promise((resolve, reject) => {
       let error = Error.IdError(uid);
-      if (error)
-        return `uid ${error}`;
+      if (error) {
+        reject(`uid ${error}`);
+        return;
+      }
 
       Users.All().then(users => {
         for (let i = 0; i < users.length; ++i) {
@@ -105,10 +113,348 @@ class Users {
       }).catch(reject);
     });
   }
+
+  static Get(id) {
+    // Return user obj
+  }
 }
 
 //-------------------------------------
+// COMMANDS
 
+class Commands {
+  static W() { // displays users currently logged in and their process along with load averages.
+    return new Promise((resolve, reject) => {
+      EXECUTE.Local('w', []).then(output => {
+        if (output.stderr) {
+          reject(output.stderr);
+          return;
+        }
+
+        let lines = output.stdout.trim().split('\n')
+          .filter(line => line && line != '' && line.trim() != '')
+          .map(line => line.trim());
+
+        // System info
+        let sysLine = lines[0];
+        let firstParts = sysLine.split(',')[0].split(' ')
+          .filter(part => part && part != '' && part.trim() != '')
+          .map(part => part.trim());
+
+        let i = 0;
+        let timestamp = firstParts[0];
+        let uptime = firstParts[2];
+
+        // Number of users
+        let secondParts = sysLine.split(',')[1].split(' ')
+          .filter(part => part && part != '' && part.trim() != '')
+          .map(part => part.trim());
+        let userCount = parseInt(secondParts[0]);
+
+        let thirdParts = sysLine.split(',')
+          .slice(2).join(',')
+          .split(':')[1]
+          .split(',')
+          .filter(part => part && part != '' && part.trim() != '')
+          .map(part => part.trim());
+
+        let loadAverages = [];
+        thirdParts.forEach(part => loadAverages.push(parseFloat(part)));
+
+        let info = {
+          timestamp: timestamp,
+          uptime: uptime,
+          userCount: userCount,
+          loadAverages: loadAverages
+        };
+
+        // Headers
+        let headersLine = lines[1];
+
+        let headers = headersLine.split(' ')
+          .filter(part => part && part != '' && part.trim() != '')
+          .map(part => part.trim());
+
+        // User info
+        let users = [];
+
+        let userLines = lines.slice(2)
+          .filter(line => line && line != '' && line.trim() != '')
+          .map(line => line.trim());
+
+        userLines.forEach(line => {
+          let parts = line.split(' ')
+            .filter(part => part && part != '' && part.trim() != '')
+            .map(part => part.trim());
+
+          let name = parts[0];
+          let tty = parts[1];
+          let group = parts[2];
+          let login = parts[3];
+          let idle = parts[4];
+          let jcpu = parts[5];
+          let pcpu = parts[6];
+          let what = parts.slice(7).join(' ');
+
+          let user = {
+            name: name,
+            tty: tty,
+            group: group,
+            login: login,
+            idle: idle,
+            jcpu: jcpu,
+            pcpu: pcpu,
+            what: what,
+            info: info
+          }
+          users.push(user);
+        });
+        resolve(users);
+      }).catch(reject);
+    });
+  }
+
+  static Ps() { // Gives status of running processes with a unique id called PID and other fields.
+    return new Promise((resolve, reject) => {
+      let fields = ['user', 'uid', 'gid', 'tty', 'start', 'etime', 'pid', 'ppid', 'pgid', 'tid', 'tgid', 'sid', 'ruid', 'rgid', 'euid', 'suid', 'fsuid', 'comm'];
+
+      let args = ['xao', fields.join(',')];
+      console.log(`CMD: ${args.join(' ')}`);
+      EXECUTE.Local('ps', args).then(output => {
+        if (output.stderr) {
+          reject(output.stderr);
+          return;
+        }
+
+        // Get lines
+        let lines = output.stdout.trim().split('\n')
+          .filter(line => line && line != '' && line.trim() != '')
+          .map(line => line.trim());
+
+        let headers = lines[0].split(' ')
+          .filter(line => line && line != '' && line.trim() != '')
+          .map(line => line.trim());
+
+        // Parse lines into process objects
+        let processes = [];
+
+        lines.slice(1).forEach(line => {
+          let parts = line.split(' ')
+            .filter(part => part && part != '' && part.trim() != '')
+            .map(part => part.trim());
+
+          let process = {};
+          for (let i = 0; i < fields.length; ++i) {
+            let currField = fields[i];
+            let currValue = parts[i];
+            if (currField.endsWith('id'))
+              process[currField] = parseInt(currValue);
+            else
+              process[currField] = currValue;
+          }
+          processes.push(process);
+        });
+        resolve(processes);
+      }).catch(reject);
+    });
+  }
+
+  static Kill(pid) { // Kills a process
+    return new Promise((resolve, reject) => {
+      let error = Error.IdNumberError(pid)
+      if (error) {
+        reject(error);
+        return;
+      }
+
+      let args = ['-9', pid];
+      EXECUTE.Local('kill', args).then(output => {
+        if (output.stderr) {
+          reject(output.stderr);
+          return;
+        }
+        resolve(true);
+      }).catch(reject);
+    });
+  }
+
+  static Uptime() { // Displays uptime info
+    return new Promise((resolve, reject) => {
+      EXECUTE.Local('uptime', []).then(output => {
+        if (output.stderr) {
+          reject(output.stderr);
+          return;
+        }
+
+        // System info
+        let line = output.stdout.trim();
+        let firstParts = line.split(',')[0].split(' ')
+          .filter(part => part && part != '' && part.trim() != '')
+          .map(part => part.trim());
+
+        let i = 0;
+        let timestamp = firstParts[0];
+        let uptime = firstParts[2];
+
+        // Number of users
+        let secondParts = line.split(',')[1].split(' ')
+          .filter(part => part && part != '' && part.trim() != '')
+          .map(part => part.trim());
+        let userCount = parseInt(secondParts[0]);
+
+        let thirdParts = line.split(',')
+          .slice(2).join(',')
+          .split(':')[1]
+          .split(',')
+          .filter(part => part && part != '' && part.trim() != '')
+          .map(part => part.trim());
+
+        let loadAverages = [];
+        thirdParts.forEach(part => loadAverages.push(parseFloat(part)));
+
+        let info = {
+          timestamp: timestamp,
+          uptime: uptime,
+          userCount: userCount,
+          loadAverages: loadAverages
+        };
+        resolve(info);
+      }).catch(reject);
+    });
+  }
+
+  static WhoAmI() { // Returns current username
+    return new Promise((resolve, reject) => {
+      EXECUTE.Local('whoami', []).then(output => {
+        if (output.stderr) {
+          reject(output.stderr);
+          return;
+        }
+        resolve(output.stdout.trim());
+      }).catch(reject);
+    });
+  }
+
+  static Free() { // Displays info regarding memory and resources (i.e. memory, swap, etc)
+    return new Promise((resolve, reject) => {
+      EXECUTE.Local('free', []).then(output => {
+        if (output.stderr) {
+          reject(output.stderr);
+          return;
+        }
+
+        // Get lines
+        let lines = output.stdout.trim().split('\n')
+          .filter(line => line && line != '' && line.trim() != '')
+          .map(line => line.trim());
+
+        // Get info objects
+        let findIt = function (strings, prefix) {
+          for (let i = 0; i < strings.length; ++i) {
+            let currStr = strings[i];
+            if (currStr.startsWith(pattern))
+              return currStr;
+          }
+          return null;
+        };
+
+        // Create memory object
+        let hasBuffersCacheLine = lines.find(findIt(lines, '-/+ buffers/cache:')) != null;
+
+        let memLine = lines.find(findIt(lines, 'Mem:'));
+        let memParts = memLine.split(' ')
+          .filter(part => part && part != '' && part.trim() != '')
+          .map(part => part.trim());
+
+        let memObj = {};
+        memObj.total = parseInt(parts[1].trim());
+        memObj.used = parseInt(parts[2].trim());
+        memObj.free = parseInt(parts[3].trim());
+        memObj.shared = parseInt(parts[4].trim());
+
+        if (hasBuffersCacheLine) {
+          memObj.buffers = parseInt(parts[5].trim());
+          memObj.cached = parseInt(parts[6].trim());
+        }
+        else {
+          memObj.buffCache = parseInt(parts[5].trim());
+          memObj.available = parseInt(parts[6].trim());
+        }
+
+        // Create swap object
+        let swapLine = lines.find(findIt(lines, 'Swap:'));
+        let swapParts = swapLine.split(' ');
+
+        let swapObj = {};
+
+        let buffLine = lines.find(findIt(lines, '-/+ buffers/cache:'));
+        let buffersCache = {};
+
+
+
+        let infos = [];
+
+        lines.slice(1).forEach(line => {
+          let parts = line.split(' ')
+            .filter(part => part && part != '' && part.trim() != '')
+            .map(part => part.trim());
+
+          let name = parts[0].trim().replace(':', '');
+          let total = parseInt(parts[1].trim());
+          let used = parseInt(parts[2].trim());
+          let free = parseInt(parts[3].trim());
+          let shared = parseInt(parts[4].trim());
+          let buffcache = parseInt(parts[5].trim());
+          let available = parseInt(parts[6].trim());
+
+          let info = {
+            name: name,
+            total: total,
+            used: used,
+            free: free,
+            shared: shared,
+            buffcache: buffcache,
+            available: available
+          };
+
+          infos.push(info);
+        });
+        resolve(infos);
+      }).catch(reject);
+    });
+  }
+
+  static Top() { // Shows CPU processes (1 iteration)
+    // top -n 1
+  }
+
+  static Lsof(user) { // Displays all files opened by user
+    // lsof -u user
+  }
+
+  static Last(user) { // 
+    // last [username]
+  }
+
+  static WhereIs(cmd) {  // Locate binary, sources and manual pages of the command
+    // whereis name
+  }
+
+  static Alias(name, cmdString) { // Add alias
+    // alias identifier='command';
+  }
+
+  static Unalias(name) { // Remove alias
+    // unalias name
+  }
+
+  static Ifconfig() { // Check active network interfaces
+    // ifconfig
+  }
+}
+
+//-------------------------------------
+// ADMIN
 class Admin {
   static Groups() {
     return Groups.All();
@@ -124,6 +470,30 @@ class Admin {
 
   static UserExists(uid) {
     return Users.Exists(uid);
+  }
+
+  static Uptime() {
+    return Commands.Uptime();
+  }
+
+  static Processes() {
+    return Commands.Ps();
+  }
+
+  static Kill(pid) {
+    return Commands.Kill(pid);
+  }
+
+  static WhoAmI() {
+    return Commands.WhoAmI();
+  }
+
+  static MemoryCheck() {
+    return Commands.Free();
+  }
+
+  static LoggedIn() {  // Same as 'w' command
+    return Commands.W();
   }
 }
 
@@ -155,11 +525,24 @@ class Error {
       return null;
   }
 
+  static IdNumberError(id) {
+    let idMin = 0;
+    if (!Number.isInteger(id) || id < idMin)
+      return `must be greater than or equal to ${idMin}`;
+    return null;
+  }
+
+  static IdStringError(id) {
+    let error = Error.StringError(id);
+    if (error)
+      return error;
+    return null;
+  }
+
   static IdError(id) {
     let error = Error.NullOrUndefined(id);
     if (error)
       return error;
-
 
     // Check if name string
     if (typeof id == 'string') {
@@ -176,10 +559,22 @@ class Error {
         return `must be greater than or equal to ${idMin}`;
       return null;
     }
-
     return `must be a string or an integer greater than or equal to ${idMin}`;
   }
 }
+
+//------------------------------------
+// TEST
+
+Admin.MemoryCheck().then(infos => {
+
+  console.log(`INFO:\n${JSON.stringify(infos)}`);
+
+}).catch(error => {
+  console.log(`ERROR: ${error}`);
+})
+return;
+
 
 //------------------------------------
 // EXPORTS
