c19aa263e0849e21654b29c7d92abaef7d370cd9
Jesus Isaias Arevalo <isa90201@gmail.com>
2018-02-11 22:34:59 -0800

    Removed obsolete code. Fixed bugs.

--- test/timestamp/test_timestamp.js
+++ test/timestamp/test_timestamp.js
@@ -4,89 +4,63 @@ let PATH = require('path');
 let rootDir = PATH.join(__dirname, '..', '..');
 
 let timestampJs = PATH.join(rootDir, 'timestamp.js');
-let TIMESTAMP = require(timestampJs);
+let TIMESTAMP = require(timestampJs).Timestamp;
 
 //------------------------------------------
 
 describe('*** timestamp.js ***', () => {
-  describe('Error', () => {
-    describe('NullOrUndefined(o)', () => {
-      it(`Returns 'undefined' if o is undefined.`, () => {
-        EXPECT(TIMESTAMP.Error.NullOrUndefined(undefined)).to.equal('undefined');
-      });
+  describe('Timestamp', () => {
+    let t = TIMESTAMP.Timestamp();
 
-      it(`Returns 'null' if o is null.`, () => {
-        EXPECT(TIMESTAMP.Error.NullOrUndefined(null)).to.equal('null');
-      });
-
-      it('Returns null if o is defined.', () => {
-        EXPECT(TIMESTAMP.Error.NullOrUndefined(1)).to.equal(null);
-        EXPECT(TIMESTAMP.Error.NullOrUndefined('Hai')).to.equal(null);
-        EXPECT(TIMESTAMP.Error.NullOrUndefined([])).to.equal(null);
+    describe('Timestamp()', () => {
+      it('Returns an object.', () => {
+        let success = t != null && !(t === undefined);
+        EXPECT(success).to.equal(true);
       });
     });
 
-    describe('StringError(s)', () => {
-      it(`Returns 'undefined' if s is undefined.`, () => {
-        EXPECT(TIMESTAMP.Error.StringError(undefined)).to.equal('undefined');
+    describe('MilitaryStringToMeridiemString(militaryTime)', () => {
+      it('Returns an error if militaryTime is invalid.', () => {
+        let results = TIMESTAMP.MilitaryStringToMeridiemString('');
+        EXPECT(results.error).to.not.equal(null);
       });
 
-      it(`Returns 'null' if s is null.`, () => {
-        EXPECT(TIMESTAMP.Error.StringError(null)).to.equal('null');
-      });
-
-      it(`Returns 'not a string' if s is not string type.`, () => {
-        EXPECT(TIMESTAMP.Error.StringError(1)).to.equal('not a string');
-        EXPECT(TIMESTAMP.Error.StringError([])).to.equal('not a string');
-        EXPECT(TIMESTAMP.Error.StringError(true)).to.equal('not a string');
-      });
-
-      it(`Returns 'empty' if s is empty.`, () => {
-        EXPECT(TIMESTAMP.Error.StringError('')).to.equal('empty');
-      });
-
-      it(`Returns 'whitespace' if s is all whitespace.`, () => {
-        EXPECT(TIMESTAMP.Error.StringError(' ')).to.equal('whitespace');
-        EXPECT(TIMESTAMP.Error.StringError('\t')).to.equal('whitespace');
-        EXPECT(TIMESTAMP.Error.StringError('\n  \t   ')).to.equal('whitespace');
-      });
-
-      it(`Returns null if s is valid.`, () => {
-        EXPECT(TIMESTAMP.Error.StringError('hai')).to.equal(null);
-        EXPECT(TIMESTAMP.Error.StringError('   hai')).to.equal(null);
-        EXPECT(TIMESTAMP.Error.StringError('hai \t\n\t')).to.equal(null);
+      it('Returns a meridiem time string if militaryTime is valid.', () => {
+        let results = TIMESTAMP.MilitaryStringToMeridiemString(t.militaryTime.string);
+        EXPECT(results.string).to.equal(t.meridiemTime.string);
       });
     });
 
-    describe('MeridiemTimeStringError(string)', () => { // CONT HERE
-      it(`Reports undefined strings.`, () => {
-        let error = TIMESTAMP.Error.MeridiemTimeStringError(undefined);
-        EXPECT(error).to.equal('Time string is undefined');
+    describe('MeridiemToMilitaryTime(meridiemTime)', () => {
+      it('Returns an error if meridiemTime is invalid.', () => {
+        let results = TIMESTAMP.MeridiemToMilitaryTime('');
+        EXPECT(results.error).to.not.equal(null);
       });
 
-      it(`Reports null strings.`, () => {
-        let error = TIMESTAMP.Error.MeridiemTimeStringError(null);
-        EXPECT(error).to.equal('Time string is null');
+      it('Returns a military time string if meridiemTime is valid.', () => {
+        let results = TIMESTAMP.MeridiemToMilitaryTime(t.meridiemTime.string);
+        EXPECT(results.string).to.equal(t.militaryTime.string);
+      });
+    });
+
+    describe('Difference(d1, d2)', () => {
+      let d1 = { year: 2018, month: 1, day: 1, hours: 0, minutes: 0, seconds: 0, milliseconds: 0 };
+      let d2 = { year: 2018, month: 1, day: 1, hours: 0, minutes: 0, seconds: 0, milliseconds: 100 };
+      let difference = 100;
+
+      it('Returns an error if d1 or d2 is invalid.', () => {
+        let results = TIMESTAMP.Difference(null, d2);
+        EXPECT(results.error).to.not.equal(null);
       });
 
-      it(`Reports invalid values.`, () => {
-        EXPECT(TIMESTAMP.Error.MeridiemTimeStringError('X:00:00 AM')).to.equal('Hours are not formatted correctly');
-        EXPECT(TIMESTAMP.Error.MeridiemTimeStringError('15:00:00 AM')).to.equal('Hours must be between 1 and 12');
-
-        EXPECT(TIMESTAMP.Error.MeridiemTimeStringError('10:X:00 AM')).to.equal('Minutes are not formatted correctly');
-        EXPECT(TIMESTAMP.Error.MeridiemTimeStringError('10:65:00 AM')).to.equal('Minutes must be between 0 and 59');
-
-        EXPECT(TIMESTAMP.Error.MeridiemTimeStringError('10:00:X AM')).to.equal('Seconds are not formatted correctly');
-        EXPECT(TIMESTAMP.Error.MeridiemTimeStringError('10:00:65 AM')).to.equal('Seconds must be between 0 and 59');
-
-        EXPECT(TIMESTAMP.Error.MeridiemTimeStringError('10:00:00 CA')).to.equal('Suffix AM|PM is not formatted correctly');
+      it('Returns an error if d1 or d2 is invalid.', () => {
+        let results = TIMESTAMP.Difference(d1, undefined);
+        EXPECT(results.error).to.not.equal(null);
       });
 
-      it(`Returns null if no errors detected.`, () => {
-        EXPECT(TIMESTAMP.Error.MeridiemTimeStringError('10:00:00 AM')).to.equal(null);
-        EXPECT(TIMESTAMP.Error.MeridiemTimeStringError('10:30:00 AM')).to.equal(null);
-        EXPECT(TIMESTAMP.Error.MeridiemTimeStringError('10:00:30 AM')).to.equal(null);
-        EXPECT(TIMESTAMP.Error.MeridiemTimeStringError('10:00:00 PM')).to.equal(null);
+      it('Returns number of milliseconds if d1 and d2 are valid.', () => {
+        let results = TIMESTAMP.Difference(d1, d2);
+        EXPECT(results.milliseconds).to.equal(difference);
       });
     });
   });
