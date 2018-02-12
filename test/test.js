7b060eb81195c075e7aba00176010b653bc1c143
Jesus Isaias Arevalo <isa90201@gmail.com>
2018-02-12 11:53:27 -0800

    Added admin test to main test.

--- test/test.js
+++ test/test.js
@@ -12,35 +12,47 @@ let testDir = PATH.join(rootDir, 'test');
 //----------------------------------------------------
 // Add test files to run
 
+/*
 let testExecuteDir = PATH.join(testDir, 'execute');
-mocha.addFile(PATH.join(testExecuteDir, 'test_execute.js'));
+mocha.addFile(PATH.join(testExecuteDir, 'test_execute.js'));*/
 
+/*
 let testTimestampDir = PATH.join(testDir, 'timestamp');
-mocha.addFile(PATH.join(testTimestampDir, 'test_timestamp.js'));
+mocha.addFile(PATH.join(testTimestampDir, 'test_timestamp.js'));*/
 
+/*
 let testPathDir = PATH.join(testDir, 'path');
-mocha.addFile(PATH.join(testPathDir, 'test_path.js'));
+mocha.addFile(PATH.join(testPathDir, 'test_path.js'));*/
 
+/*
 let testListDir = PATH.join(testDir, 'list');
-mocha.addFile(PATH.join(testListDir, 'test_list.js'));
+mocha.addFile(PATH.join(testListDir, 'test_list.js'));*/
 
+/*
 let testPermissionsDir = PATH.join(testDir, 'permissions');
-mocha.addFile(PATH.join(testPermissionsDir, 'test_permissions.js'));
+mocha.addFile(PATH.join(testPermissionsDir, 'test_permissions.js'));*/
 
-let testCopyDir = PATH.join(testDir, 'copy');
-mocha.addFile(PATH.join(testCopyDir, 'test_copy.js'));
+/*let testCopyDir = PATH.join(testDir, 'copy');
+mocha.addFile(PATH.join(testCopyDir, 'test_copy.js'));*/
 
+/*
 let testRemoveDir = PATH.join(testDir, 'remove');
-mocha.addFile(PATH.join(testRemoveDir, 'test_remove.js'));
+mocha.addFile(PATH.join(testRemoveDir, 'test_remove.js'));*/
 
+/*
 let testMkdirDir = PATH.join(testDir, 'mkdir');
-mocha.addFile(PATH.join(testMkdirDir, 'test_mkdir.js'));
+mocha.addFile(PATH.join(testMkdirDir, 'test_mkdir.js'));*/
 
+/*
 let testMoveDir = PATH.join(testDir, 'move');
-mocha.addFile(PATH.join(testMoveDir, 'test_move.js'));
+mocha.addFile(PATH.join(testMoveDir, 'test_move.js'));*/
 
+/*
 let testRsyncDir = PATH.join(testDir, 'rsync');
-mocha.addFile(PATH.join(testRsyncDir, 'test_rsync.js'));
+mocha.addFile(PATH.join(testRsyncDir, 'test_rsync.js'));*/
+
+let testAdminDir = PATH.join(testDir, 'admin');
+mocha.addFile(PATH.join(testAdminDir, 'test_admin.js'));
 
 let runner = mocha.run(() => { });
 
@@ -56,13 +68,14 @@ function importTest(name, path) {
 //-------------------------------
 // TESTS
 
-importTest('execute.js', PATH.join(rootDir, 'execute.js'));
-importTest('timestamp.js', PATH.join(rootDir, 'timestamp.js'));
-importTest('path.js', PATH.join(rootDir, 'path.js'));
-importTest('list.js', PATH.join(rootDir, 'list.js'));
-importTest('permissions.js', PATH.join(rootDir, 'permissions.js'));
-importTest('copy.js', PATH.join(rootDir, 'copy.js'));
-importTest('remove.js', PATH.join(rootDir, 'remove.js'));
-importTest('mkdir.js', PATH.join(rootDir, 'mkdir.js'));
-importTest('move.js', PATH.join(rootDir, 'move.js'));
-importTest('rsync.js', PATH.join(rootDir, 'rsync.js'));
+//importTest('execute.js', PATH.join(rootDir, 'execute.js'));
+//importTest('timestamp.js', PATH.join(rootDir, 'timestamp.js'));
+//importTest('path.js', PATH.join(rootDir, 'path.js'));
+//importTest('list.js', PATH.join(rootDir, 'list.js'));
+//importTest('permissions.js', PATH.join(rootDir, 'permissions.js'));
+//importTest('copy.js', PATH.join(rootDir, 'copy.js'));
+//importTest('remove.js', PATH.join(rootDir, 'remove.js'));
+//importTest('mkdir.js', PATH.join(rootDir, 'mkdir.js'));
+//importTest('move.js', PATH.join(rootDir, 'move.js'));
+//importTest('rsync.js', PATH.join(rootDir, 'rsync.js'));
+importTest('admin.js', PATH.join(rootDir, 'admin.js'));
