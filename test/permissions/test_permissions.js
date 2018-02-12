b01fda20f39683e1848e450ccc26650fbbef015f
Jesus Isaias Arevalo <isa90201@gmail.com>
2018-02-11 21:35:32 -0800

    Removed obsolete code. Fixed bugs.

--- test/permissions/test_permissions.js
+++ test/permissions/test_permissions.js
@@ -9,46 +9,6 @@ let PERMISSIONS = require(permissionsJs);
 //------------------------------------------
 
 describe('*** permissions.js ***', () => {
-  describe('Error', () => {
-    describe('NullOrUndefined(o)', () => {
-      it(`Returns 'undefined' if o is undefined.`, () => {
-        EXPECT(PERMISSIONS.Error.NullOrUndefined(undefined)).to.equal('undefined');
-      });
-
-      it(`Returns 'null' if o is null.`, () => {
-        EXPECT(PERMISSIONS.Error.NullOrUndefined(null)).to.equal('null');
-      });
-
-      it('Returns null if o is defined.', () => {
-        EXPECT(PERMISSIONS.Error.NullOrUndefined(1)).to.equal(null);
-        EXPECT(PERMISSIONS.Error.NullOrUndefined('Hai')).to.equal(null);
-        EXPECT(PERMISSIONS.Error.NullOrUndefined([])).to.equal(null);
-      });
-    });
-
-    describe('IntegerError(i)', () => {
-      it(`Returns 'undefined' if i is undefined.`, () => {
-        EXPECT(PERMISSIONS.Error.IntegerError(undefined)).to.equal('is undefined');
-      });
-
-      it(`Returns 'null' if i is null.`, () => {
-        EXPECT(PERMISSIONS.Error.IntegerError(null)).to.equal('is null');
-      });
-
-      it(`Returns 'is not an integer' if i is not an integer type.`, () => {
-        EXPECT(PERMISSIONS.Error.IntegerError('string')).to.equal('is not an integer');
-        EXPECT(PERMISSIONS.Error.IntegerError([])).to.equal('is not an integer');
-        EXPECT(PERMISSIONS.Error.IntegerError(true)).to.equal('is not an integer');
-      });
-
-      it(`Returns null if i is a valid integer between 0 and 7.`, () => {
-        EXPECT(PERMISSIONS.Error.IntegerError(0)).to.equal(null);
-        EXPECT(PERMISSIONS.Error.IntegerError(5 - 4)).to.equal(null);
-        EXPECT(PERMISSIONS.Error.IntegerError(7)).to.equal(null);
-      });
-    });
-  });
-
   describe('Permissions', () => {
     let invalidPermStr = 'r--r--r-- 12 root root 4096 Jan 1 14:30 file.txt';
     let validPermStr = 'r--r--r--';
@@ -81,8 +41,7 @@ describe('*** permissions.js ***', () => {
 
       it('Returns an object if permStr is valid.', () => {
         let results = PERMISSIONS.Permissions.CreatePermissionsObjectUsingPermissionsString(validPermStr);
-        let error = PERMISSIONS.Error.ObjectError(results.obj, false);
-        EXPECT(error).to.equal(null);
+        EXPECT(results.error).to.equal(null);
       });
     });
 
@@ -98,8 +57,7 @@ describe('*** permissions.js ***', () => {
 
       it('Returns an object if octalStr is valid.', () => {
         let results = PERMISSIONS.Permissions.CreatePermissionsObjectUsingOctalString(validOctalStr);
-        let error = PERMISSIONS.Error.ObjectError(results.obj, false);
-        EXPECT(error).to.equal(null);
+        EXPECT(results.error).to.equal(null);
       });
     });
 
