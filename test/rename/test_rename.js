ee6ac7aafd37f26e486f05484049500b90d28044
Jesus Isaias Arevalo <isa90201@gmail.com>
2018-02-13 14:42:52 -0800

    fixed bugs.

--- test/rename/test_rename.js
+++ test/rename/test_rename.js
@@ -12,190 +12,43 @@ let MKDIR = require(mkdirJs).Mkdir;
 let removeJs = _path.join(rootDir, 'remove.js');
 let REMOVE = require(removeJs).Remove;
 
+let pathJs = _path.join(rootDir, 'path.js');
+let PATH = require(pathJs).Path;
+
 //------------------------------------------
 
-describe('*** chmod.js ***', () => {
-  describe('Chmod', () => {
-    let testDirPath = _path.join(rootDir, 'delete_this_test_dir');
+describe('*** rename.js ***', () => {
+  describe('Rename', () => {
+    describe('Rename(currPath, newName)', () => {
+      let testDirPath = _path.join(rootDir, 'delete_this_test_dir');
+      let newName = 'new_test_dir';
 
-    describe('UsingPermString(permStr, path)', () => {
-      let validPermString = 'rwxrwxrwx';
-      let invalidPermString = 'blah';
-
-      it('Returns error if permStr is undefined.', () => {
-        CHMOD.UsingPermString(null, testDirPath).then(success => EXPECT(false))
+      it('Returns error if currPath is undefined.', () => {
+        RENAME.Rename(null, newName).then(success => EXPECT(false))
           .catch(error => EXPECT(error).to.not.equal(null));
       });
 
-      it('Returns error if permStr is invalid.', () => {
-        CHMOD.UsingPermString(invalidPermString, testDirPath).then(success => EXPECT(false))
+      it('Returns error if currPath is invalid.', () => {
+        RENAME.Rename('', newName).then(success => EXPECT(false))
           .catch(error => EXPECT(error).to.not.equal(null));
       });
 
-      it('Returns error if path is undefined.', () => {
-        CHMOD.UsingPermString(validPermString, null).then(success => EXPECT(false))
+      it('Returns error if newName is undefined.', () => {
+        RENAME.Rename(testDirPath, null).then(success => EXPECT(false))
           .catch(error => EXPECT(error).to.not.equal(null));
       });
 
-      it('Actually changes permissions.', () => {
+      it('Returns error if newName is invalid.', () => {
+        RENAME.Rename(testDirPath, '').then(success => EXPECT(false))
+          .catch(error => EXPECT(error).to.not.equal(null));
+      });
+
+      it('Actually renames stuff.', () => {
         MKDIR.Mkdirp(testDirPath).then(success => {
-          CHMOD.UsingPermString(validPermString, testDirPath).then(success => {
-            REMOVE.Directory(testDirPath).then(success => EXPECT(true))
-              .catch(error => EXPECT(false));
-          }).catch(error => EXPECT(false));
-        }).catch(error => EXPECT(false));
-      });
-    });
-
-    describe('UsingOctalString(octalStr, path)', () => {
-      let validOctalStr = '777';
-      let invalidOctalStr = '77A';
-
-      it('Returns error if octalStr is undefined.', () => {
-        CHMOD.UsingOctalString(null, testDirPath).then(success => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns error if octalStr is invalid.', () => {
-        CHMOD.UsingPermString(invalidOctalStr, testDirPath).then(success => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns error if path is undefined.', () => {
-        CHMOD.UsingOctalString(validOctalStr, null).then(success => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Actually changes permissions.', () => {
-        MKDIR.Mkdirp(testDirPath).then(success => {
-          CHMOD.UsingOctalString(validOctalStr, testDirPath).then(success => {
-            REMOVE.Directory(testDirPath).then(success => EXPECT(true))
-              .catch(error => EXPECT(false));
-          }).catch(error => EXPECT(false));
-        }).catch(error => EXPECT(false));
-      });
-    });
-
-    describe('RemovePermissions(classes, types, path)', () => {
-      let validClasses = 'ugo';
-      let invalidClasses = 'abc';
-
-      let validTypes = 'rwx';
-      let invalidTypes = 'abc';
-
-      it('Returns error if classes is undefined.', () => {
-        CHMOD.RemovePermissions(null, validTypes, testDirPath).then(success => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns error if classes is invalid.', () => {
-        CHMOD.RemovePermissions(invalidClasses, validTypes, testDirPath).then(success => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns error if types is undefined.', () => {
-        CHMOD.RemovePermissions(validClasses, undefined, testDirPath).then(success => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns error if types is invalid.', () => {
-        CHMOD.RemovePermissions(validClasses, invalidTypes, testDirPath).then(success => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns error if path is undefined.', () => {
-        CHMOD.RemovePermissions(validClasses, validTypes, null).then(success => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Actually changes permissions.', () => {
-        MKDIR.Mkdirp(testDirPath).then(success => {
-          CHMOD.RemovePermissions(validClasses, validTypes, testDirPath).then(success => {
-            REMOVE.Directory(testDirPath).then(success => EXPECT(true))
-              .catch(error => EXPECT(false));
-          }).catch(error => EXPECT(false));
-        }).catch(error => EXPECT(false));
-      });
-    });
-
-    describe('AddPermissions(classes, types, path)', () => {
-      let validClasses = 'ugo';
-      let invalidClasses = 'abc';
-
-      let validTypes = 'rwx';
-      let invalidTypes = 'abc';
-
-      it('Returns error if classes is undefined.', () => {
-        CHMOD.AddPermissions(null, validTypes, testDirPath).then(success => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns error if classes is invalid.', () => {
-        CHMOD.AddPermissions(invalidClasses, validTypes, testDirPath).then(success => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns error if types is undefined.', () => {
-        CHMOD.AddPermissions(validClasses, undefined, testDirPath).then(success => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns error if types is invalid.', () => {
-        CHMOD.AddPermissions(validClasses, invalidTypes, testDirPath).then(success => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns error if path is undefined.', () => {
-        CHMOD.AddPermissions(validClasses, validTypes, null).then(success => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Actually changes permissions.', () => {
-        MKDIR.Mkdirp(testDirPath).then(success => {
-          CHMOD.AddPermissions(validClasses, validTypes, testDirPath).then(success => {
-            REMOVE.Directory(testDirPath).then(success => EXPECT(true))
-              .catch(error => EXPECT(false));
-          }).catch(error => EXPECT(false));
-        }).catch(error => EXPECT(false));
-      });
-    });
-
-    describe('SetPermissions(classes, types, path)', () => {
-      let validClasses = 'ugo';
-      let invalidClasses = 'abc';
-
-      let validTypes = 'rwx';
-      let invalidTypes = 'abc';
-
-      it('Returns error if classes is undefined.', () => {
-        CHMOD.SetPermissions(null, validTypes, testDirPath).then(success => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns error if classes is invalid.', () => {
-        CHMOD.SetPermissions(invalidClasses, validTypes, testDirPath).then(success => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns error if types is undefined.', () => {
-        CHMOD.SetPermissions(validClasses, undefined, testDirPath).then(success => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns error if types is invalid.', () => {
-        CHMOD.SetPermissions(validClasses, invalidTypes, testDirPath).then(success => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns error if path is undefined.', () => {
-        CHMOD.SetPermissions(validClasses, validTypes, null).then(success => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Actually changes permissions.', () => {
-        MKDIR.Mkdirp(testDirPath).then(success => {
-          CHMOD.SetPermissions(validClasses, validTypes, testDirPath).then(success => {
-            REMOVE.Directory(testDirPath).then(success => EXPECT(true))
+          RENAME.Rename(testDirPath, newName).then(success => {
+            let parentDir = PATH.Path.ParentDir(testDirPath);
+            let renamedPath = _path.join(parentDir.dir, newName);
+            REMOVE.Directory(renamedPath).then(success => EXPECT(true))
               .catch(error => EXPECT(false));
           }).catch(error => EXPECT(false));
         }).catch(error => EXPECT(false));
