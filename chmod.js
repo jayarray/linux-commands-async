0860be301ea1cd0e51d9698465919b88d280eaf9
Jesus Isaias Arevalo <isa90201@gmail.com>
2018-02-13 12:53:07 -0800

    Fixed bugs. Added error checks.

--- chmod.js
+++ chmod.js
@@ -1,6 +1,6 @@
 let PERMISSIONS = require('./permissions.js').Permissions;
 let ERROR = require('./error.js').Error;
-let PATH = require('./path.js');
+let PATH = require('./path.js').Path;
 let FS = require('fs-extra');
 
 //-----------------------------------------
@@ -8,44 +8,63 @@ let FS = require('fs-extra');
 class Chmod {
   static UsingPermString(permStr, path) {
     return new Promise((resolve, reject) => {
+      let error = ERROR.StringError(permStr);
+      if (error) {
+        reject(`Permissions string is ${error}`);
+        return;
+      }
+
       PATH.Exists(path).then(exists => {
         if (!exists) {
           reject(`Path does not exist: ${path}`);
           return;
         }
 
-        PERMISSIONS.CreatePermissionsObjectUsingPermissionsString(permStr.trim()).then(obj => {
-          let octalNumber = parseInt(obj.octal.string);
-          FS.chmod(path, octalNumber, (err) => {
-            if (err) {
-              reject(`Failed to change permissions: ${err}`);
-              return;
-            }
-            resolve(true);
-          });
-        }).catch(reject);
+        let permsObj = PERMISSIONS.CreatePermissionsObjectUsingPermissionsString(permStr.trim());
+        if (permsObj.error) {
+          reject(permsObj.error);
+          return;
+        }
+
+        FS.chmod(path, permsObj.obj.octal.string, (err) => {
+          if (err) {
+            reject(`Failed to change permissions: ${err}`);
+            return;
+          }
+          resolve(true);
+        });
+
       }).catch(reject);
     });
   }
 
   static UsingOctalString(octalStr, path) {
     return new Promise((resolve, reject) => {
+      let error = ERROR.StringError(octalStr);
+      if (error) {
+        reject(`Octal string is ${error}`);
+        return;
+      }
+
       PATH.Exists(path).then(exists => {
         if (!exists) {
           reject(`Path does not exist: ${path}`);
           return;
         }
 
-        PERMISSIONS.CreatePermissionsObjectUsingOctalString(octalStr.trim()).then(obj => {
-          let octalNumber = parseInt(obj.octal.string);
-          FS.chmod(path, octalNumber, (err) => {
-            if (err) {
-              reject(`Failed to change permissions: ${err}`);
-              return;
-            }
-            resolve(true);
-          });
-        }).catch(reject);
+        let permsObj = PERMISSIONS.CreatePermissionsObjectUsingOctalString(octalStr.trim());
+        if (permsObj.error) {
+          reject(permsObj.error);
+          return;
+        }
+
+        FS.chmod(path, permsObj.obj.octal.string, (err) => {
+          if (err) {
+            reject(`Failed to change permissions: ${err}`);
+            return;
+          }
+          resolve(true);
+        });
       }).catch(reject);
     });
   }
@@ -85,8 +104,7 @@ class Chmod {
         }
 
         // Change permissions
-        let octalNumber = parseInt(octalStr.string);
-        FS.chmod(path, octalNumber, (err) => {
+        FS.chmod(path, octalStr.string, (err) => {
           if (err) {
             reject(`Failed to change permissions: ${err}`);
             return;
@@ -132,8 +150,7 @@ class Chmod {
         }
 
         // Change permissions
-        let octalNumber = parseInt(octalStr.string);
-        FS.chmod(path, octalNumber, (err) => {
+        FS.chmod(path, octalStr.string, (err) => {
           if (err) {
             reject(`Failed to change permissions: ${err}`);
             return;
@@ -160,10 +177,12 @@ class Chmod {
 
       PERMISSIONS.Permissions(path).then(permsObj => {
         // Modify permsObj
-        classes.split('').forEach(c => {
-          let typesList = Chmod.ValidTypeChars();
+        let classesList = Chmod.ValidClassChars();
+        let typesList = Chmod.ValidTypeChars();
+
+        classesList.forEach(c => {
           typesList.forEach(t => {
-            if (types.includes(t))
+            if (classes.includes(c) && types.includes(t))
               permsObj[c][t] = true;
             else
               permsObj[c][t] = false;
@@ -171,9 +190,33 @@ class Chmod {
         });
 
         // Create new permStr based on modified permsObj
-        let newPermStr = `${permsObj.u.r ? 'r' : '-'}${permsObj.u.w ? 'w' : '-'}${permsObj.u.x ? permsObj.u.xchar : '-'}`;
-        newPermStr += `${permsObj.g.r ? 'r' : '-'}${permsObj.g.w ? 'w' : '-'}${permsObj.g.x ? permsObj.g.xchar : '-'}`;
-        newPermStr += `${permsObj.o.r ? 'r' : '-'}${permsObj.o.w ? 'w' : '-'}${permsObj.o.x ? permsObj.o.xchar : '-'}`;
+        let ux = '';
+        if (permsObj.u.x == true && permsObj.u.xchar == '-')
+          ux = 'x';
+        else if (permsObj.u.x == true && permsObj.u.xchar != '-')
+          ux = permsObj.u.xchar;
+        else
+          ux = '-';
+
+        let gx = '';
+        if (permsObj.g.x == true && permsObj.g.xchar == '-')
+          gx = 'x';
+        else if (permsObj.g.x == true && permsObj.g.xchar != '-')
+          gx = permsObj.g.xchar;
+        else
+          gx = '-';
+
+        let ox = '';
+        if (permsObj.o.x == true && permsObj.o.xchar == '-')
+          ox = 'x';
+        else if (permsObj.o.x == true && permsObj.o.xchar != '-')
+          ox = permsObj.o.xchar;
+        else
+          ox = '-';
+
+        let newPermStr = `${permsObj.u.r ? 'r' : '-'}${permsObj.u.w ? 'w' : '-'}${ux}`;
+        newPermStr += `${permsObj.g.r ? 'r' : '-'}${permsObj.g.w ? 'w' : '-'}${gx}`;
+        newPermStr += `${permsObj.o.r ? 'r' : '-'}${permsObj.o.w ? 'w' : '-'}${ox}`;
 
         let octalStr = PERMISSIONS.PermissionsStringToOctalString(newPermStr);
         if (octalStr.error) {
@@ -182,8 +225,7 @@ class Chmod {
         }
 
         // Change permissions
-        let octalNumber = parseInt(octalStr.string);
-        FS.chmod(path, octalNumber, (err) => {
+        FS.chmod(path, octalStr.string, (err) => {
           if (err) {
             reject(`Failed to change permissions: ${err}`);
             return;
@@ -210,19 +252,19 @@ class Error {
   static ClassesStringError(string) {
     let error = ERROR.StringError(string);
     if (error)
-      return `Class string is ${error}`;
+      return `Classes string is ${error}`;
 
     let min = 1;
     let max = 3;
 
     // Check length
     if (string.length < min || string.length > max)
-      return `Class string must have ${min} to ${max} characters`;
+      return `Classes string must have ${min} to ${max} characters`;
 
     // Check for invalid chars
     for (let i = 0; i < string.length; ++i) {
       let currChar = string.charAt(i);
-      if (!Chmod.ValidClassChars().includes(char))
+      if (!Chmod.ValidClassChars().includes(currChar))
         return `Classes string contains invalid characters`;
     }
 
@@ -244,7 +286,7 @@ class Error {
     // Check for invalid chars
     for (let i = 0; i < string.length; ++i) {
       let currChar = string.charAt(i);
-      if (!Chmod.ValidTypeChars().includes(char))
+      if (!Chmod.ValidTypeChars().includes(currChar))
         return `Types string contains invalid characters`;
     }
 
