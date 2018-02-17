c4db27030e1ffb6deb6309253d9d0aaff5b4a11f
Jesus Isaias Arevalo <isa90201@gmail.com>
2018-02-17 14:21:13 -0800

    Major refactoring in PAth class to reflect changes done in the Execute class. Must provide executor to have linux command executed (instead of using FS commands).

--- path.js
+++ path.js
@@ -1,86 +1,163 @@
 let PATH = require('path');
 let FS = require('fs-extra');
-let ERROR = require('./error.js').Error;
+let ERROR = require('./error.js');
+let COMMAND = require('./command.js').Command;
+let REMOTE_COMMAND = require('./command.js').RemoteCommand;
 
 //-----------------------------------
 // PATH
 class Path {
-  static Exists(path) {
+  static Exists(path, remoteExecutor = null) {
     return new Promise((resolve, reject) => {
-      let error = Error.PathError(path);
-      if (error) {
-        reject(error);
+      let pathError = Error.PathValidator(path);
+      if (pathError) {
+        reject(`Failed to check if path exists: ${pathError}`);
         return;
       }
 
-      FS.access(path, FS.F_OK, (err) => {
-        if (err)
-          resolve(false);
-        else
-          resolve(true);
-      });
+      if (remoteExecutor === undefined) {
+        reject(`Failed to check if path exists: Connection is undefined`);
+        return;
+      }
+
+      if (remoteExecutor == null) { // Local (uses FS)
+        FS.access(path, FS.F_OK, (err) => {
+          if (err)
+            resolve(false);
+          else
+            resolve(true);
+          return;
+        });
+      }
+      else {
+        let cmdStr = CommandStringBuiler.Exists(path);
+
+        COMMAND.Execute(cmdStr, [], remoteExecutor).then(output => {
+          if (output.stderr) {
+            reject(`Failed to check if path exists: ${output.stderr}`);
+            return;
+          }
+
+          let value = parseInt(output.stdout.trim());
+          resolve(value == 1);  // 1 is true, 0 is false
+        }).catch(error => {
+          reject(`Failed to check if path exists: ${error}`);
+        });
+      }
     });
   }
 
-  static IsFile(path) {
+  static IsFile(path, remoteExecutor = null) {
     return new Promise((resolve, reject) => {
-      Path.Exists(path).then(exists => {
-        if (!exists) {
-          reject(`Path does not exist: ${path}`);
-          return;
-        }
+      if (remoteExecutor == null) {
+        Path.Exists(path).then(exists => {
+          if (!exists) {
+            reject(`Failed to check if local path is a file: Path does not exist: ${path}`);
+            return;
+          }
 
-        FS.lstat(path, (err, stats) => {
-          if (err)
-            reject(err);
-          else
-            resolve(stats.isFile() && !stats.isDirectory());
+          FS.lstat(path, (err, stats) => {
+            if (err)
+              reject(err);
+            else
+              resolve(stats.isFile() && !stats.isDirectory());
+          });
+        }).catch(error => {
+          reject(`Failed to check if local path is a file: ${error}`);
         });
-      }).catch(reject);
+      }
+      else {
+        Path.Exists(path, remoteExecutor).then(exists => {
+          if (!exists) {
+            reject(`Failed to check if remote path is a file: Path does not exist: ${path}`);
+            return;
+          }
+
+          let cmdStr = CommandStringBuiler.IsFile(path);
+
+          COMMAND.Execute(cmdStr, [], remoteExecutor).then(output => {
+            if (output.stderr) {
+              reject(`Failed to check if remote path is a file: ${output.stderr}`);
+              return;
+            }
+
+            let value = parseInt(output.stdout.trim());
+            resolve(value == 1);
+          }).catch(error => {
+            reject(`Failed to check if remote path is a file: ${error}`);
+          });
+        }).catch();
+      }
     });
   }
 
-  static IsDir(path) {
+  static IsDir(path, remoteExecutor = null) {
     return new Promise((resolve, reject) => {
-      Path.Exists(path).then(exists => {
-        if (!exists) {
-          reject(`Path does not exist: ${path}`);
-          return;
-        }
+      if (remoteExecutor == null) {
+        Path.Exists(path).then(exists => {
+          if (!exists) {
+            reject(`Failed to check if local path is a directory: Path does not exist: ${path}`);
+            return;
+          }
 
-        FS.lstat(path, (err, stats) => {
-          if (err)
-            reject(err);
-          else
-            resolve(stats.isDirectory());
+          FS.lstat(path, (err, stats) => {
+            if (err)
+              reject(err);
+            else
+              resolve(stats.isDirectory());
+          });
+        }).catch(error => {
+          reject(`Failed to check if local path is a directory: ${error}`);
         });
-      }).catch(reject);
+      }
+      else {
+        Path.Exists(path, remoteExecutor).then(exists => {
+          if (!exists) {
+            reject(`Failed to check if remote path is a directory: Path does not exist: ${path}`);
+            return;
+          }
+
+          let cmdStr = CommandStringBuiler.IsDir(path);
+
+          COMMAND.Execute(cmdStr, [], remoteExecutor).then(output => {
+            if (output.stderr) {
+              reject(`Failed to check if remote path is a directory: ${output.stderr}`);
+              return;
+            }
+
+            let value = parseInt(output.stdout.trim());
+            resolve(value == 1);
+          }).catch(error => {
+            reject(`Failed to check if remote path is a directory: ${error}`);
+          });
+        }).catch();
+      }
     });
   }
 
   static Filename(path) {
-    let error = Error.PathError(path);
+    let error = Error.PathValidator(path);
     if (error)
       return { name: null, error: error };
     return { name: PATH.basename(path), error: null };
   }
 
   static Extension(path) {
-    let error = Error.PathError(path);
+    let error = Error.PathValidator(path);
     if (error)
       return { extension: null, error: error };
     return { extension: PATH.extname(path), error: null };
   }
 
   static ParentDirName(path) {
-    let error = Error.PathError(path);
+    let error = Error.PathValidator(path);
     if (error)
       return { name: null, error: error };
     return { name: PATH.dirname(path).split(PATH.sep).pop(), error: null };
   }
 
   static ParentDir(path) {
-    let error = Error.PathError(path);
+    let error = Error.PathValidator(path);
     if (error)
       return { dir: null, error: error };
     return { dir: PATH.dirname(path), error: null }; // Full path to parent dir
@@ -102,11 +179,28 @@ class Path {
 }
 
 //---------------------------------
-// ERROR
+// COMMAND STRING BUILDER
+
+class CommandStringBuiler {
+  static Exists(path) {
+    return `if [ -e ${path} ]; then echo 1; else echo 0; fi`;
+  }
+
+  static IsFile(path) {
+    return `if [ -f ${path} ]; then echo 1; else echo 0; fi`;
+  }
+
+  static IsDir(path) {
+    return `if [ -d ${path} ]; then echo 1; else echo 0; fi`;
+  }
+}
+
+//---------------------------------
+// HELPERS
 
 class Error {
-  static PathError(p) {
-    let error = ERROR.StringError(p);
+  static PathValidator(p) {
+    let error = ERROR.StringValidator(p);
     if (error)
       return `Path is ${error}`;
     return null;
@@ -114,7 +208,24 @@ class Error {
 }
 
 //------------------------------------
+// TEST
+
+let dirPath = '/home/pi/python';
+let filePath = '/home/isa/test.txt';
+
+let user = 'pi';
+let host = 'teagirl';
+let remoteExecutor = new REMOTE_COMMAND(user, host)
+
+Path.IsDir(dirPath, remoteExecutor).then(exists => {
+  console.log(`EXISTS: ${exists}`);
+}).catch(error => {
+  console.log(`ERROR: ${error}`);
+});
+
+//------------------------------------
 // EXPORTS
 
 exports.Path = Path;
+exports.CommandStringBuiler = CommandStringBuiler;
 exports.Error = Error;
