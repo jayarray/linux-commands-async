f1be0fc80f82a7204e20abc851c3d7244816edbe
Jesus Isaias Arevalo <isa90201@gmail.com>
2018-02-20 16:58:15 -0800

    Removed fs-extra import. Updated code to use command class instead of executed.

--- copy.js
+++ copy.js
@@ -1,39 +1,98 @@
 let PATH = require('./path.js');
-let FS = require('fs-extra');
+let ERROR = require('./error.js');
+let COMMAND = require('./command.js').Command;
+let LINUX_COMMANDS = require('./linuxcommands.js');
 
 //-------------------------------------------------
 // COPY (cp)
 class Copy {
-  static Copy(src, dest) {
+  static File(src, dest, executor) {
     return new Promise((resolve, reject) => {
-      let error = PATH.Error.PathError(src);
-      if (error) {
-        reject(`Source error: ${error}`);
+      let srcError = PATH.Error.PathValidator(src);
+      if (srcError) {
+        reject(`Failed to copy file: Source is ${srcError}`);
         return;
       }
 
-      error = PATH.Error.PathError(dest);
-      if (error) {
-        reject(`Destination error: ${error}`);
+      let destError = PATH.Error.PathValidator(dest);
+      if (destError) {
+        reject(`Failed to copy file: Destination is ${destError}`);
         return;
       }
 
-      PATH.Path.exists(src).then(exists => {
+      let executorError = ERROR.ExecutorValidator(executor);
+      if (executorError) {
+        reject(`Failed to copy file: Connection is ${executorError}`);
+        return;
+      }
+
+      PATH.Path.Exists(src, executor).then(exists => {
         if (!exists) {
-          reject(`Source error: Path does not exist: ${src}`);
+          reject(`Failed to copy file: Source does not exist: ${src}`);
           return;
         }
 
-        FS.copy(src, dest, (err) => {
-          if (err) {
-            reject(`Copy failed: ${err}`);
+        PATH.Path.IsFile(src, executor).then(isFile => {
+          if (!isFile) {
+            reject(`Failed to copy file: Source is not a file: ${src}`);
             return;
           }
-          resolve(true);
-        });
-      }).catch(error => {
-        reject(`Source error: ${error}`);
-      });
+
+          let cmd = LINUX_COMMANDS.CopyFile(src, dest);
+          COMMAND.Execute(cmd, [], executor).then(output => {
+            if (output.stderr) {
+              reject(`Failed to copy file: ${output.stderr}`);
+              return;
+            }
+            resolve(true);
+          }).catch(error => reject(`Failed to copy file: ${error}`));
+        }).catch(error => reject(`Failed to copy file: ${error}`));
+      }).catch(error => reject(`Failed to copy file: ${error}`));
+    });
+  }
+
+  static Direcory(src, dest, executor) {
+    return new Promise((resolve, reject) => {
+      let srcError = PATH.Error.PathValidator(src);
+      if (srcError) {
+        reject(`Failed to copy directory: Source is ${srcError}`);
+        return;
+      }
+
+      let destError = PATH.Error.PathValidator(dest);
+      if (destError) {
+        reject(`Failed to copy directory: Destination is ${destError}`);
+        return;
+      }
+
+      let executorError = ERROR.ExecutorValidator(executor);
+      if (executorError) {
+        reject(`Failed to copy directory: Connection is ${executorError}`);
+        return;
+      }
+
+      PATH.Path.Exists(src, executor).then(exists => {
+        if (!exists) {
+          reject(`Failed to copy directory: Source does not exist: ${src}`);
+          return;
+        }
+
+        PATH.Path.IsDir(src, executor).then(isFile => {
+          if (!isFile) {
+            reject(`Failed to copy directory: Source is not a directory: ${src}`);
+            return;
+          }
+
+          let cmd = LINUX_COMMANDS.CopyDir(src, dest);
+          COMMAND.Execute(cmd, [], executor).then(output => {
+            if (output.stderr) {
+              reject(`Failed to copy directory: ${output.stderr}`);
+              return;
+            }
+            resolve(true);
+          }).catch(error => reject(`Failed to copy directory: ${error}`));
+        }).catch(error => reject(`Failed to copy directory: ${error}`));
+      }).catch(error => reject(`Failed to copy directory: ${error}`));
     });
   }
 }
