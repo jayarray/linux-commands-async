fac0c365c6f66b6fe9154dd447fd85152ceb0caf
Jesus Isaias Arevalo <isa90201@gmail.com>
2018-02-20 17:26:53 -0800

    Removed fs-extra import. Updated code to use command class instead of execute.

--- move.js
+++ move.js
@@ -1,36 +1,45 @@
 let PATH = require('./path.js');
-let FS = require('fs-extra');
+let ERROR = require('./error.js');
+let COMMAND = require('./command.js').Command;
+let LINUX_COMMANDS = require('./linuxcommands.js');
 
 //------------------------------------------------------
 // MOVE 
 class Move {
-  static Move(src, dest) {
+  static Move(src, dest, executor) {
     return new Promise((resolve, reject) => {
-      let error = PATH.Error.PathError(src);
-      if (error) {
-        reject(`Source error: ${error}`);
+      let srcError = PATH.Error.PathValidator(src);
+      if (srcError) {
+        reject(`Failed to move: Source is ${srcError}`);
         return;
       }
 
-      error = PATH.Error.PathError(dest);
-      if (error) {
-        reject(`Destination error: ${error}`);
+      let destError = PATH.Error.PathValidator(dest);
+      if (destError) {
+        reject(`Failed to move: Destination is ${destError}`);
         return;
       }
 
-      PATH.Path.Exists(src).then(exists => {
+      let executorError = ERROR.ExecutorValidator(executor);
+      if (executorError) {
+        reject(`Failed to move: Connection is ${executorError}`);
+        return;
+      }
+
+      PATH.Path.Exists(src, executor).then(exists => {
         if (!exists) {
-          reject(`Source error: Path does not exist: ${src}`);
+          reject(`Failed to move: Source does not exist: ${src}`);
           return;
         }
 
-        FS.move(src, dest, (err) => {
-          if (err) {
-            reject(`Failed to move path: ${err}`);
+        let cmd = LINUX_COMMANDS.Move(src, dest);
+        COMMAND.Execute(cmd, [], executor).then(output => {
+          if (output.stderr) {
+            reject(`Failed to move: ${output.stderr}`);
             return;
           }
           resolve(true);
-        });
+        }).catch(error => reject(`Failed to move: ${error}`));
       }).catch(reject);
     });
   }
