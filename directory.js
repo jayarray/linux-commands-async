665225f08991697c673d2aa53354676a5346f111
Jesus Isaias Arevalo <isa90201@gmail.com>
2018-02-20 18:55:11 -0800

    Updated code to reflect changes made to imported classes.

--- directory.js
+++ directory.js
@@ -2,38 +2,51 @@ let REMOVE = require('./remove.js').Remove;
 let MKDIR = require('./mkdir.js').Mkdir;
 let PATH = require('./path.js');
 let DISKUSAGE = require('./diskusage.js').DiskUsage;
+let ERROR = require('./error.js');
 
 //-----------------------------------------
 // DIRECTORY
 
 class Directory {
-  static Remove(path) {
-    return REMOVE.Directory(path);
+  static Remove(path, executor) {
+    return REMOVE.Directory(path, executor);
   }
 
-  static Create(path) {
+  static Create(path, executor) {
     return new Promise((resolve, reject) => {
-      let error = PATH.Error.PathError(path);
-      if (error) {
-        reject(error);
+      let pathError = PATH.Error.PathError(path);
+      if (pathError) {
+        reject(`Failed to create directory: ${pathError}`);
         return;
       }
 
-      MKDIR.Mkdirp(path).then(success => {
+      let executorError = ERROR.ExecutorValidator(executor);
+      if (executorError) {
+        reject(`Failed to create directory: Connection is ${executorError}`);
+        return;
+      }
+
+      MKDIR.Mkdirp(path, executor).then(success => {
         resolve(true);
       }).catch(reject);
     });
   }
 
-  static Size(path) {
+  static Size(path, executor) {
     return new Promise((resolve, reject) => {
-      let error = PATH.Error.PathError(path);
-      if (error) {
-        reject(error);
+      let pathError = PATH.Error.PathError(path);
+      if (pathError) {
+        reject(`Failed to get directory size: ${pathError}`);
         return;
       }
 
-      DISKUSAGE.DirSize(path).then(size => {
+      let executorError = ERROR.ExecutorValidator(executor);
+      if (executorError) {
+        reject(`Failed to get directory size: Connection is ${executorError}`);
+        return;
+      }
+
+      DISKUSAGE.DirSize(path, executor).then(size => {
         resolve(size);
       }).catch(reject);
     });
