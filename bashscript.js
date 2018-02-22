1bed393bf093acd964fc5895c8a48523bdd235f3
Jesus Isaias Arevalo <isa90201@gmail.com>
2018-02-21 21:04:49 -0800

    Replaced execute with command class. Updated code to reflect changes made. Made errors more explicit.

--- bashscript.js
+++ bashscript.js
@@ -1,29 +1,66 @@
 let FILE = require('./file.js').File;
-let EXECUTE = require('./execute.js').Execute;
+let COMMAND = require('./command.js').Command;
+let ERROR = require('./error.js');
 
 //----------------------------------------
 // BASH SCRIPT
 
 class BashScript {
-  static Create(path, content) {
+  static Create(path, content, executor) {
     return new Promise((resolve, reject) => {
-      FILE.Create(path, `#!/bin/bash\n${content}`).then(success => {
-        FILE.MakeExecutable(path).then(success => {
+      let error = PATH.Error.PathValidator(path);
+      if (error) {
+        reject(`Failed to create bash script: ${error}`);
+        return;
+      }
+
+      error = ERROR.StringValidator(content);
+      if (error == 'not a string') {
+        reject(`Failed to create bash script: content is ${error}`);
+        return;
+      }
+
+      let executorError = ERROR.ExecutorValidator(executor);
+      if (executorError) {
+        reject(`Failed to create bashscript: Connection is ${executorError}`);
+        return;
+      }
+
+      FILE.Create(path, `#!/bin/bash\n${content}`, executor).then(success => {
+        FILE.MakeExecutable(path, executor).then(success => {
           resolve(true);
-        }).catch(reject);
-      }).catch(reject);
+        }).catch(error => `Failed to create bashscript: ${error}`);
+      }).catch(error => `Failed to create bashscript: ${error}`);
     });
   }
 
-  static Execute(path, content) {
+  static Execute(path, content, executor) {
     return new Promise((resolve, reject) => {
-      BashScript.Create(path, content).then(success => {
-        EXECUTE.Local(path, []).then(output => {
+      let error = PATH.Error.PathValidator(path);
+      if (error) {
+        reject(`Failed to execute bash script: ${error}`);
+        return;
+      }
+
+      error = ERROR.StringValidator(content);
+      if (error == 'not a string') {
+        reject(`Failed to execute bash script: content is ${error}`);
+        return;
+      }
+
+      let executorError = ERROR.ExecutorValidator(executor);
+      if (executorError) {
+        reject(`Failed to execute bashscript: Connection is ${executorError}`);
+        return;
+      }
+
+      BashScript.Create(path, content, executor).then(success => {
+        COMMAND.Execute(path, [], executor).then(output => {
           resolve(output.stdout);
-          FILE.Remove(path).then(success => {
-          }).catch(reject);
-        }).catch(reject);
-      }).catch(reject);
+          FILE.Remove(path, executor).then(success => {
+          }).catch(error => `Failed to execute bash script: ${error}`);
+        }).catch(error => `Failed to execute bash script: ${error}`);
+      }).catch(error => `Failed to execute bash script: ${error}`);
     });
   }
 }
