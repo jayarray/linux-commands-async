c10b7ab49d3cb2e9adabbe759c9aac2512a37a45
Jesus Isaias Arevalo <isa90201@gmail.com>
2018-02-26 13:23:38 -0800

    Removed unused imports. Updated code to reflect changes made after removing error class.

--- directory.js
+++ directory.js
@@ -2,50 +2,40 @@ let REMOVE = require('./remove.js').Remove;
 let MKDIR = require('./mkdir.js').Mkdir;
 let PATH = require('./path.js');
 let DISKUSAGE = require('./diskusage.js').DiskUsage;
-let ERROR = require('./error.js');
+let VALIDATE = require('./validate.js');
 
 //-----------------------------------------
 // DIRECTORY
 
 class Directory {
   static Remove(path, executor) {
-    return REMOVE.Directory(path, executor);
+    return REMOVE.Directories([path], executor);
   }
 
   static Create(path, executor) {
+    let pathError = VALIDATE.IsStringInput(path);
+    if (pathError)
+      return Promise.reject(`Failed to create directory: ${pathError}`);
+
+    if (!executor)
+      return Promise.reject(`Failed to create directory: Executor is required`);
+
     return new Promise((resolve, reject) => {
-      let pathError = PATH.Error.PathValidator(path);
-      if (pathError) {
-        reject(`Failed to create directory: ${pathError}`);
-        return;
-      }
-
-      let executorError = ERROR.ExecutorValidator(executor);
-      if (executorError) {
-        reject(`Failed to create directory: Connection is ${executorError}`);
-        return;
-      }
-
       MKDIR.Mkdirp(path, executor).then(success => {
         resolve(true);
-      }).catch(reject);
+      }).catch(error => `Failed tp create directory: ${error}`);
     });
   }
 
   static Size(path, executor) {
+    let pathError = VALIDATE.IsStringInput(path);
+    if (pathError)
+      return Promise.reject(`Failed to get directory size: ${pathError}`);
+
+    if (!executor)
+      return Promise.reject(`Failed to get directory size: Executor is required`);
+
     return new Promise((resolve, reject) => {
-      let pathError = PATH.Error.PathValidator(path);
-      if (pathError) {
-        reject(`Failed to get directory size: ${pathError}`);
-        return;
-      }
-
-      let executorError = ERROR.ExecutorValidator(executor);
-      if (executorError) {
-        reject(`Failed to get directory size: Connection is ${executorError}`);
-        return;
-      }
-
       DISKUSAGE.DirSize(path, executor).then(size => {
         resolve(size);
       }).catch(reject);
