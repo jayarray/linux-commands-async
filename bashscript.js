68b9f158fe408460ad50c8627b2b74712b9d2d74
Jesus Isaias Arevalo <isa90201@gmail.com>
2018-02-26 12:57:04 -0800

    Removed unused imports. Updated code to reflect changes made after removing error class.

--- bashscript.js
+++ bashscript.js
@@ -1,31 +1,23 @@
 let FILE = require('./file.js').File;
-let COMMAND = require('./command.js').Command;
-let ERROR = require('./error.js');
+let VALIDATE = require('./validate.js');
 
 //----------------------------------------
 // BASH SCRIPT
 
 class BashScript {
   static Create(path, content, executor) {
+    let pathError = VALIDATE.IsStringInput(path);
+    if (pathError)
+      return Promise.reject(`Failed to create bash script: path is ${pathError}`);
+
+    let contentError = VALIDATE.IsStringInput(content);
+    if (error == 'not a string')
+      return Promise.reject(`Failed to create bash script: content is ${error}`);
+
+    if (!executor)
+      return Promise.reject(`Failed to create bashscript: Connection is ${executorError}`);
+
     return new Promise((resolve, reject) => {
-      let error = PATH.Error.PathValidator(path);
-      if (error) {
-        reject(`Failed to create bash script: ${error}`);
-        return;
-      }
-
-      error = ERROR.StringValidator(content);
-      if (error == 'not a string') {
-        reject(`Failed to create bash script: content is ${error}`);
-        return;
-      }
-
-      let executorError = ERROR.ExecutorValidator(executor);
-      if (executorError) {
-        reject(`Failed to create bashscript: Connection is ${executorError}`);
-        return;
-      }
-
       FILE.Create(path, `#!/bin/bash\n${content}`, executor).then(success => {
         FILE.MakeExecutable(path, executor).then(success => {
           resolve(true);
@@ -35,27 +27,20 @@ class BashScript {
   }
 
   static Execute(path, content, executor) {
+    let pathError = VALIDATE.IsStringInput(path);
+    if (pathError)
+      return Promise.reject(`Failed to execute bash script: path is ${pathError}`);
+
+    let contentError = VALIDATE.IsStringInput(content);
+    if (error == 'not a string')
+      return Promise.reject(`Failed to execute bash script: content is ${error}`);
+
+    if (!executor)
+      return Promise.reject(`Failed to execute bashscript: Connection is ${executorError}`);
+
     return new Promise((resolve, reject) => {
-      let error = PATH.Error.PathValidator(path);
-      if (error) {
-        reject(`Failed to execute bash script: ${error}`);
-        return;
-      }
-
-      error = ERROR.StringValidator(content);
-      if (error == 'not a string') {
-        reject(`Failed to execute bash script: content is ${error}`);
-        return;
-      }
-
-      let executorError = ERROR.ExecutorValidator(executor);
-      if (executorError) {
-        reject(`Failed to execute bashscript: Connection is ${executorError}`);
-        return;
-      }
-
       BashScript.Create(path, content, executor).then(success => {
-        COMMAND.Execute(path, [], executor).then(output => {
+        executor.Execute(path, []).then(output => {
           resolve(output.stdout);
           FILE.Remove(path, executor).then(success => {
           }).catch(error => `Failed to execute bash script: ${error}`);
