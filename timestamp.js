a1192f121e0a0688d6e29c111b7d41545cd4bb13
Jesus Isaias Arevalo <isa90201@gmail.com>
2018-02-27 12:39:43 -0800

    Reduced Timestamp to only show current timestamp info.

--- timestamp.js
+++ timestamp.js
@@ -1,411 +1,99 @@
-let VALIDATE = require('./validate.js');
-
-//-------------------------------------
-
 class Timestamp {
-  static Timestamp() {
-    let d = new Date();
+  static Now(executor) {
+    return new Promise((resolve, reject) => {
+      // BUILD COMMANDS
+      let dayInfoHeaders = ['abbr', 'name', 'weekNumber', 'monthNumber']; // weekNumber => 1-7 (1=Mon), monthNumber => 01-31
+      let dayInfoCmd = `date +'%a %A %u %d'`;
 
-    // TIME
-    let hours = d.getHours();  // 0-23
-    let minutes = d.getMinutes();  // 0-59
-    let seconds = d.getSeconds();  // 0-59
-    let milliseconds = d.getMilliseconds();  // 0-999 
+      let weekInfoHeaders = ['number']; // number => 00-53
+      let weekInfoCmd = `date +'%U'`;
 
-    let hoursStr = `00${hours}`.slice(-2);
+      let monthInfoHeaders = ['abbr', 'name', 'number'];  // number => 01-12
+      let monthInfoCmd = `date +'%b %B %m'`;
 
-    let minutesStr = `00${minutes}`;
-    minutesStr = minutesStr.slice(-2);
+      let yearInfoHeaders = ['abbr', 'full']; // abbr => 00-99
+      let yearInfoCmd = `date +'%y %Y'`;
 
-    let secondsStr = `00${seconds}`;
-    secondsStr = secondsStr.slice(-2);
+      let meridiemTimeInfoHeaders = ['hours', 'minutes', 'seconds', 'nanoseconds', 'suffix']; // hours => 00-23, minutes/seconds => 00-59, nanoseconds => 000000000-999999999
+      let meridiemTimeInfoCmd = `date +'%I %M %S %N %P'`; // NOTE: %P (i.e. suffix) may not be known in some places.
 
-    let militaryTime = {  // 24-hour format
-      hours: hours,
-      minutes: minutes,
-      seconds: seconds,
-      milliseconds: milliseconds,
-      string: `${hoursStr}:${minutesStr}:${secondsStr}`
-    }
-
-    let adjustedHours = null;
-    let suffix = null;
-
-    if (hours == 0) {
-      adjustedHours = 12;
-      suffix = 'AM';
-    }
-    else if (hours == 12) {
-      adjustedHours = 12;
-      suffix = 'PM';
-    }
-    else if (hours > 12) {
-      adjustedHours = hours % 12;
-      suffix = 'PM';
-    }
-    else {
-      adjustedHours = hours;
-      suffix = 'AM';
-    }
-
-    hoursStr = `00${adjustedHours}`.slice(-2);
-
-    let timeStr = `${hoursStr}:${minutesStr}:${secondsStr} ${suffix}`;
-
-    let meridiemTime = {  // 12-hour format (AM | PM)
-      hours: adjustedHours,
-      minutes: minutes,
-      seconds: seconds,
-      milliseconds: milliseconds,
-      suffix: suffix,
-      string: timeStr
-    }
-
-    // DATE
-    let year = d.getFullYear();  // yyyy
-
-    let monthNumber = d.getMonth(); // 0-11;
-    let monthName = Timestamp.Months()[monthNumber];
-    let dayOfMonth = d.getDate(); // 1-31
-
-    let dayOfWeekNumber = d.getDay();  // 0-6
-    let dayOfWeekName = Timestamp.DaysOfTheWeek()[dayOfWeekNumber];
-
-    return {
-      hours: hours,
-      minutes: minutes,
-      seconds: seconds,
-      milliseconds: milliseconds,
-      militaryTime: militaryTime,
-      meridiemTime: meridiemTime,
-      year: year,
-      monthNumber: monthNumber,
-      monthName: monthName,
-      dayOfMonth: dayOfMonth,
-      dayOfWeekNumber: dayOfWeekNumber,
-      dayOfWeekName: dayOfWeekName
-    };
-  }
-
-  static MilitaryStringToMeridiemString(militaryTime) {
-    let error = Error.MilitaryTimeStringError(militaryTime);
-    if (error)
-      return { string: null, error: error };
-
-    let parts = militaryTime.trim().split(':');
-
-    let hoursStr = parts[0];
-    let hoursVal = parseInt(hoursStr);
-
-    let minutesStr = parts[1];
-    let minutesVal = parseInt(minutesStr);
-
-    let secondsStr = parts[2];
-    let secondsVal = parseInt(secondsStr);
-
-    let adjustedHours = null;
-    if (hoursVal == 0 || hoursVal == 12) {
-      adjustedHours = 12;
-    }
-    else if (hoursVal > 12) {
-      adjustedHours = hoursVal % 12;
-    }
-    else {
-      adjustedHours = hoursVal;
-    }
-
-    let timeStr = `${adjustedHours}:${minutesStr}:${secondsStr}`;
-    if (hoursVal < 12) {
-      timeStr += ' AM';
-    }
-    else {
-      timeStr += ' PM';
-    }
-    return { string: timeStr, error: null };
-  }
-
-  static MeridiemToMilitaryTime(meridiemTime) {
-    let error = Error.MeridiemTimeStringError(meridiemTime);
-    if (error)
-      return { string: null, error: error };
-
-    let parts = meridiemTime.split(':');
-
-    let hoursStr = parts[0];
-    let hoursVal = parseInt(hoursStr);
-
-    let minutesStr = parts[1];
-    let minutesVal = parseInt(minutesStr);
-
-    let secondsStr = parts[2].split(' ')[0].trim();
-    let secondsVal = parseInt(secondsStr);
-
-    let adjustedHours = null;
-    if (meridiemTime.includes('AM') && hoursVal == 12) {
-      adjustedhours = 0;
-    }
-    else if (meridiemTime.includes('PM') && hoursVal < 12) {
-      adjustedHours = hoursVal + 12;
-    }
-    else {
-      adjustedHours = hoursVal;
-    }
-    return { string: `${adjustedHours}:${minutesStr}:${secondsStr}`, error: null };
-  }
-
-  static Difference(d1, d2) {
-    let error = Error.DateObjectError(d1);
-    if (error)
-      return { milliseconds: null, error: `DATE1_OBJ_ERROR: ${error}` };
-
-    error = Error.DateObjectError(d2);
-    if (error)
-      return { milliseconds: null, error: `DATE2_OBJ_ERROR: ${error}` };
-
-    let date1 = new Date(d1.year, d1.month, d1.day, d1.hours, d1.minutes, d1.seconds, d1.milliseconds);
-    let date2 = new Date(d2.year, d2.month, d2.day, d2.hours, d2.minutes, d2.seconds, d2.milliseconds);
-    return ({ milliseconds: date2.getTime() - date1.getTime(), error: null });
-  }
-
-  static Months() {
-    return ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  }
-
-  static MonthIndex(name) {
-    let error = VALIDATE.IsStringInput(name);
-    if (error)
-      return { index: null, error: `MONTH_NAME_ERROR: Name is ${error}` };
-
-    if (Timestamp.Months().includes(name))
-      return { index: months.indexOf(name), error: null };
-    return { index: null, error: `MONTH_NAME_ERROR: Name is not a valid name` };
-  }
-
-  static DaysOfTheWeek() {
-    return ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  }
-
-  static DayOfTheWeekIndex(name) {
-    let error = VALIDATE.IsStringInput(name);
-    if (error)
-      return { index: null, error: `DAY_OF_WEEK_INDEX_ERROR: Name is ${error}` };
-
-    if (Timestamp.DaysOfTheWeek().includes(name))
-      return { index: daysOfTheWeek.indexOf(name), error: null };
-    return { index: null, error: `DAY_OF_WEEK_INDEX_ERROR: Name is not a valid name` };
-  }
-}
-
-//------------------------------------
-// ERROR
-
-class Error {
-  static MeridiemTimeStringError(string) {
-    let error = VALIDATE.IsStringInput(string);
-    if (error)
-      return `Time string is ${error}`;
-
-    let sTrimmed = string.trim();
-    let parts = sTrimmed.split(' ');  // parts = ['HH:MM:SS', '(AM|PM)']
-    if (parts.length == 2) {
-      let unitParts = parts[0].trim().split(':');
-      if (unitParts.length == 3) {
-        let hMin = 1;
-        let hMax = 12;
-        let mMin = 0;
-        let mMax = 59;
-        let sMin = mMin;
-        let sMax = mMax;
-
-        let hStr = unitParts[0].trim();
-        if (hStr.length == 2) {
-          let hours = null;
-          try {
-            hours = parseInt(hStr);
-          } catch (err) {
-            return 'Hours do not resolve to an integer';
-          }
-
-          if (hours < hMin || hours > hMax)
-            return `Hours must be between ${hMin} and ${hMax}`;
-
-          let mStr = unitParts[1].trim();
-          if (mStr.length == 2) {
-            let minutes = null;
-            try {
-              minutes = parseInt(mStr);
-            } catch (err) {
-              return 'Minutes do not resolve to an integer';
-            }
-
-            if (minutes < mMin || minutes > mMax)
-              return `Minutes must be between ${mMin} and ${mMax}`;
-
-            let sStr = unitParts[2].trim();
-            if (sStr.length == 2) {
-              let seconds = null;
-              try {
-                seconds = parseInt(sStr);
-              } catch (err) {
-                return 'Seconds do not resolve to an integer';
-              }
-
-              if (seconds < sMin || seconds > sMax)
-                return `Seconds must be between ${sMin} and ${sMax}`;
-
-              let suffix = parts[1].trim();
-              if (suffix == 'AM' || suffix == 'PM')
-                return null;
-              else
-                return 'Suffix AM|PM is not formatted correctly';
-            }
-            else
-              return 'Seconds are not formatted correctly';
-          }
-          else
-            return 'Minutes are not formatted correctly';
+      // EXECUTE COMMANDS
+      let cmdList = [dayInfoCmd, weekInfoCmd, monthInfoCmd, yearInfoCmd, meridiemTimeInfoCmd];
+      let cmdListStr = cmdList.join('\n');
+      executor.Execute(cmdListStr, []).then(output => {
+        if (output.stderr) {
+          reject(`Failed to get timestamp: ${output.stderr}`);
+          return;
         }
+
+        // PROCESS OUTPUTS
+        let lines = output.stdout.split('\n').filter(line => line && line != '' && line.trim() != '').map(line => line.trim());
+
+        // Day
+        let dayInfoParts = lines[0].split(' ').map(part => part.trim());
+        let day = {};
+        day[dayInfoHeaders[0]] = dayInfoParts[0];
+        day[dayInfoHeaders[1]] = dayInfoParts[1];
+        day[dayInfoHeaders[2]] = parseInt(dayInfoParts[2]);
+        day[dayInfoHeaders[3]] = parseInt(dayInfoParts[3]);
+
+        // Week
+        let weekInfoParts = lines[1].split(' ').map(part => part.trim());
+        let week = {};
+        week[weekInfoHeaders[0]] = parseInt(weekInfoParts[0]);
+
+        // Month
+        let monthInfoParts = lines[2].split(' ').map(part => part.trim());
+        let month = {};
+        month[monthInfoHeaders[0]] = monthInfoParts[0];
+        month[monthInfoHeaders[1]] = monthInfoParts[1];
+        month[monthInfoHeaders[2]] = parseInt(monthInfoParts[2]);
+
+        // Year
+        let yearInfoParts = lines[3].split(' ').map(part => part.trim());
+        let year = {};
+        year[yearInfoHeaders[0]] = yearInfoParts[0];
+        year[yearInfoHeaders[1]] = yearInfoParts[1];
+
+        // Meridiem Time
+        let meridiemTimeInfoParts = lines[4].split(' ').map(part => part.trim());
+        let meridiemTime = {};
+        meridiemTime[meridiemTimeInfoHeaders[0]] = parseInt(meridiemTimeInfoParts[0]);
+        meridiemTime[meridiemTimeInfoHeaders[1]] = parseInt(meridiemTimeInfoParts[1]);
+        meridiemTime[meridiemTimeInfoHeaders[2]] = parseInt(meridiemTimeInfoParts[2]);
+        meridiemTime[meridiemTimeInfoHeaders[3]] = parseInt(meridiemTimeInfoParts[3]);
+        meridiemTime[meridiemTimeInfoHeaders[4]] = meridiemTimeInfoParts[4];
+        meridiemTime['string'] = `${meridiemTimeInfoParts[0]}:${meridiemTimeInfoParts[1]}:${meridiemTimeInfoParts[2]} ${meridiemTimeInfoParts[4]}`;
+
+        // Military Time
+        let militaryTime = {};
+
+        let adjustedHours = null;
+        if (meridiemTime.suffix == 'pm' && meridiemTime.hours < 12)
+          adjustedHours = (meridiemTime.hours + 12) % 24;
+        else if (meridiemTime.suffix == 'am' && meridiemTime.hours == 12)
+          adjustedHours = 0;
         else
-          return 'Hours are not formatted correctly';
-      }
-    }
-    return 'Time string is not formatted correctly. Must follow format HH:MM:SS (AM|PM)';
-  }
+          adjustedHours = meridiemTime.hours;
 
-  static MilitaryTimeStringError(string) {
-    let error = VALIDATE.IsStringInput(string);
-    if (error)
-      return `Time string is ${error}`;
+        let hoursStr = `00${adjustedHours}`.substring(-2);
 
-    let sTrimmed = string.trim();
-    let parts = sTrimmed.split(':');  // parts = ['HH', 'MM', 'SS [(AM|PM)]'] <-- Check for suffix in SECONDS string
-    if (parts.length == 3) {
-      let hMin = 0;
-      let hMax = 23;
-      let mMin = 0;
-      let mMax = 59;
-      let sMin = mMin;
-      let sMax = mMax;
+        militaryTime['hours'] = hoursStr;
+        militaryTime['minutes'] = meridiemTime.minutes;
+        militaryTime['seconds'] = meridiemTime.seconds;
+        militaryTime['nanoseconds'] = meridiemTime.nanoseconds;
+        militaryTime['string'] = `${militaryTime.hours}:${meridiemTimeInfoParts[1]}:${meridiemTimeInfoParts[2]}`;
 
-      let hStr = parts[0].trim();
-      if (hStr.length == 2) {
-        let hours = null;
-        try {
-          hours = parseInt(hStr);
-        } catch (err) {
-          return 'Hours do not resolve to an integer';
-        }
-
-        if (hours < hMin || hours > hMax)
-          return `Hours must be between ${hMin} and ${hMax}`;
-
-        let mStr = parts[1].trim();
-        if (mStr.length == 2) {
-          let minutes = null;
-          try {
-            minutes = parseInt(mStr);
-          } catch (err) {
-            return 'Minutes do not resolve to an integer';
-          }
-
-          if (minutes < mMin || minutes > mMax)
-            return `Minutes must be between ${mMin} and ${mMax}`;
-
-          let sStr = parts[2].trim();
-          let sParts = sStr.split(' ');
-          if (sParts.length == 2)
-            return 'Trailing chars at end of time string';
-
-          if (sStr.length == 2) {
-            let seconds = null;
-            try {
-              seconds = parseInt(sStr);
-            } catch (err) {
-              return 'Seconds do not resolve to an integer';
-            }
-
-            if (seconds < sMin || seconds > sMax)
-              return `Seconds must be between ${sMin} and ${sMax}`;
-            return null;
-          }
-          else
-            return 'Seconds are not formatted correctly';
-        }
-        else
-          return 'Minutes are not formatted correctly';
-      }
-      else
-        return 'Hours are not formatted correctly';
-
-    }
-    return 'Time string is not formatted correctly. Must follow format HH:MM:SS';
-  }
-
-  static DateObjectError(dateObj) {
-    let error = VALIDATE.IsInstance(dateObj);
-    if (error)
-      return `Date object is ${error}`;
-
-    // Check if obj missing values
-    if (
-      dateObj.year === undefined ||
-      dateObj.month === undefined ||
-      dateObj.day === undefined ||
-      dateObj.hours === undefined ||
-      dateObj.minutes === undefined ||
-      dateObj.seconds === undefined ||
-      dateObj.milliseconds === undefined
-    )
-      return 'Date object is missing required values';
-
-    // Check if obj values are all integers and in range
-    if (
-      !Number.isInteger(dateObj.year) ||
-      !Number.isInteger(dateObj.month) ||
-      !Number.isInteger(dateObj.day) ||
-      !Number.isInteger(dateObj.hours) ||
-      !Number.isInteger(dateObj.minutes) ||
-      !Number.isInteger(dateObj.seconds) ||
-      !Number.isInteger(dateObj.milliseconds)
-    )
-      return 'Date object values must all be integers';
-
-    // Check if numbers are in range
-    let monthMin = 1;
-    let monthMax = 12;
-    let dayMin = 1;
-    let dayMax = 31;
-    let hoursMin = 0;
-    let hoursMax = 23;
-    let minutesMin = 0;
-    let minutesMax = 59;
-    let secondsMin = 0;
-    let secondsMax = 59;
-    let millisecondsMin = 0;
-    let millisecondsMax = 999;
-
-    if (dateObj.year < 0)
-      return 'Year must be integer greater than or equal to 0';
-
-    if (dateObj.month < monthMin || dateObj.month > monthMax)
-      return `Month must be integer between ${monthMin} and ${monthMax}`;
-
-    if (dateObj.day < dayMin || dateObj.day > dayMax)
-      return `Day must be integer between ${dayMin} and ${dayMax}`;
-
-    if (dateObj.hours < hoursMin || dateObj.hours > hoursMax)
-      return `Hours must be integer between ${hoursMin} and ${hoursMax}`;
-
-    if (dateObj.minutes < minutesMin || dateObj.minutes > minutesMax)
-      return `Minutes must be integer between ${minutesMin} and ${minutesMax}`;
-
-    if (dateObj.seconds < secondsMin || dateObj.seconds > secondsMax)
-      return `Seconds must be integer between ${secondsMin} and ${secondsMax}`;
-
-    if (dateObj.milliseconds < millisecondsMin || dateObj.milliseconds > millisecondsMax)
-      return `Milliseconds must be integer between ${millisecondsMin} and ${millisecondsMax}`;
-
-    return null;
+        resolve({
+          day: day,
+          week: week,
+          month: month,
+          year: year,
+          militaryTime: militaryTime,
+          meridiemTime: meridiemTime
+        });
+      }).catch(error => `Failed to get timestamp: ${error}`);
+    });
   }
 }
 
