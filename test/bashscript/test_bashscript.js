cf7311aa41aacc999d0c6d2e8c4e80a26a9c68b6
Jesus Arevalo <isa90201@gmail.com>
2018-03-01 15:12:04 -0800

    Updated test for bashscript.

--- test/bashscript/test_bashscript.js
+++ test/bashscript/test_bashscript.js
@@ -4,49 +4,55 @@ let PATH = require('path');
 let rootDir = PATH.join(__dirname, '..', '..');
 
 let bashscriptJs = PATH.join(rootDir, 'bashscript.js');
-let BASHSCRIPT = require(bashscriptJs).BashScript;
+let BASHSCRIPT = require(bashscriptJs);
 
 let fileJs = PATH.join(rootDir, 'file.js');
-let FILE = require(fileJs).File;
+let FILE = require(fileJs);
+
+let commandJs = PATH.join(rootDir, 'command.js');
+let COMMAND = require(commandJs);
 
 //------------------------------------------
 
 describe('*** bashscript.js ***', () => {
+  let executor = COMMAND.LOCAL;
+
   describe('BashScript', () => {
     let filepath = PATH.join(rootDir, 'delete_this_test_file.sh');
     let content = 'echo 1';
 
-    describe('Create(path, content)', () => {
+    describe('Create(path, content, executor)', () => {
       it('Returns error if path is invalid.', () => {
-        BASHSCRIPT.Create(null, content).then(success => EXPECT(false))
+        BASHSCRIPT.Create(null, content, executor).then(success => EXPECT(false))
           .catch(error => EXPECT(error).to.not.equal(null));
       });
 
       it('Returns error if content is invalid.', () => {
-        BASHSCRIPT.Create(filepath, null).then(success => EXPECT(false))
+        BASHSCRIPT.Create(filepath, null, executor).then(success => EXPECT(false))
           .catch(error => EXPECT(error).to.not.equal(null));
       });
 
-      it('Actually creates a file.', () => {
-        BASHSCRIPT.Create(filepath, content).then(success => EXPECT(true))
-          .catch(error => EXPECT(false));
+      it('Returns error if executor is invalid.', () => {
+        BASHSCRIPT.Create(filepath, content, null).then(success => EXPECT(false))
+          .catch(error => EXPECT(error).to.not.equal(null));
       });
     });
 
-    describe('Execute(path, content)', () => {
+
+    describe('Execute(path, content, executor)', () => {
       it('Returns error if path is invalid.', () => {
-        BASHSCRIPT.Execute(null, content).then(output => EXPECT(false))
+        BASHSCRIPT.Execute(null, content, executor).then(output => EXPECT(false))
           .catch(error => EXPECT(error).to.not.equal(null));
       });
 
       it('Returns error if content is invalid.', () => {
-        BASHSCRIPT.Execute(filepath, null).then(output => EXPECT(false))
+        BASHSCRIPT.Execute(filepath, null, executor).then(output => EXPECT(false))
           .catch(error => EXPECT(error).to.not.equal(null));
       });
 
-      it('Creates bash script, executes it, and cleans it up.', () => {
-        BASHSCRIPT.Execute(filepath, content).then(output => EXPECT(true))
-          .catch(error => EXPECT(false));
+      it('Returns error if executor is invalid.', () => {
+        BASHSCRIPT.Execute(filepath, content, null).then(success => EXPECT(false))
+          .catch(error => EXPECT(error).to.not.equal(null));
       });
     });
   });
