fb5b8ed2f29b488f0fcd738084530f1d9ebcc4f6
Jesus Isaias Arevalo <isa90201@gmail.com>
2018-03-01 09:49:26 -0800

    Updatede code to reflect changes made after moving function outside of class.

--- rename.js
+++ rename.js
@@ -1,34 +1,32 @@
 let _path = require('path');
 
-let PATH = require('./path.js').Path;
-let MOVE = require('./move.js').Move;
+let PATH = require('./path.js');
+let MOVE = require('./move.js');
 let VALIDATE = require('./validate.js');
 
 //-----------------------------------------------
 // RENAME
 
-class Rename {
-  static Rename(src, newName, executor) {
-    let srcError = VALIDATE.IsStringInput(src);
-    if (srcError)
-      return Promise.reject(`Failed to rename: Source is ${srcError}`);
+function Rename(src, newName, executor) {
+  let srcError = VALIDATE.IsStringInput(src);
+  if (srcError)
+    return Promise.reject(`Failed to rename: Source is ${srcError}`);
 
-    let newNameIsValid = typeof newName == 'string' && newName != '';
-    if (!newNameIsValid)
-      return Promise.reject(`Failed to rename: New name must be a string type and cannot be empty`);
+  let newNameIsValid = typeof newName == 'string' && newName != '';
+  if (!newNameIsValid)
+    return Promise.reject(`Failed to rename: New name must be a string type and cannot be empty`);
 
-    if (!executor)
-      return Promise.reject(`Failed to rename: Executor is required`);
+  if (!executor)
+    return Promise.reject(`Failed to rename: Executor is required`);
 
-    return new Promise((resolve, reject) => {
-      let parentDir = PATH.ParentDir(src);
-      let dest = _path.join(parentDir, newName);
+  return new Promise((resolve, reject) => {
+    let parentDir = PATH.ParentDir(src);
+    let dest = _path.join(parentDir, newName);
 
-      MOVE.Move(src, dest, executor).then(success => {
-        resolve(true);
-      }).catch(error => reject(`Failed to rename: ${error}`));
-    });
-  }
+    MOVE.Move(src, dest, executor).then(success => {
+      resolve(true);
+    }).catch(error => reject(`Failed to rename: ${error}`));
+  });
 }
 
 //------------------------------------
