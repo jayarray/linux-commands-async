aff65f9b3ebfcf885ee3ec75b66a249c00b66f03
Jesus Isaias Arevalo <isa90201@gmail.com>
2018-03-01 08:50:01 -0800

    Updated code to reflect changes made after moving functions out of their classes.

--- chmod.js
+++ chmod.js
@@ -3,296 +3,298 @@ let VALIDATE = require('./validate.js');
 
 //-----------------------------------------
 // CHMOD
-class Chmod {
-  static UsingPermString(permStr, paths, isRecursive, executor) {
-    let permStrError = VALIDATE.IsStringInput(permStr);
-    if (permStrError)
-      return Promise.reject(`Failed to change permissions: Permissions string is ${permStrError}`);
 
-    let pathsError = Error.PathsValidator(paths);
-    if (pathsError)
-      return Promise.reject(`Failed to change permissions: ${pathsError}`);
+function UsingPermString(permStr, paths, isRecursive, executor) {
+  let permStrError = VALIDATE.IsStringInput(permStr);
+  if (permStrError)
+    return Promise.reject(`Failed to change permissions: Permissions string is ${permStrError}`);
 
-    if (!executor)
-      return Promise.reject(`Failed to change permissions: Executor is required`);
+  let pathsError = PathsValidator(paths);
+  if (pathsError)
+    return Promise.reject(`Failed to change permissions: ${pathsError}`);
 
-    let permsObj = PERMISSIONS.Permissions.CreatePermissionsObjectUsingPermissionsString(permStr.trim());
-    if (permsObj.error)
-      return Promise.reject(`Failed to change permissions: ${permsObj.error}`);
+  if (!executor)
+    return Promise.reject(`Failed to change permissions: Executor is required`);
 
-    return new Promise((resolve, reject) => {
-      let octalStr = permsObj.obj.octal.string;
+  let permsObj = PERMISSIONS.Permissions.CreatePermissionsObjectUsingPermissionsString(permStr.trim());
+  if (permsObj.error)
+    return Promise.reject(`Failed to change permissions: ${permsObj.error}`);
 
-      let args = [];
-      if (isRecursive)
-        args.push('-R');
-      args.push(octalStr);
-      args = args.concat(paths);
+  return new Promise((resolve, reject) => {
+    let octalStr = permsObj.obj.octal.string;
 
-      executor.Execute('chmod', args).then(output => {
-        if (output.stderr) {
-          reject(`Failed to change permissions: ${output.stderr}`);
-          return;
-        }
-        resolve(true);
-      }).catch(error => reject(`Failed to change permissions: ${error}`));
-    });
-  }
+    let args = [];
+    if (isRecursive)
+      args.push('-R');
+    args.push(octalStr);
+    args = args.concat(paths);
 
-  static UsingOctalString(octalStr, paths, isRecursive, executor) {
-    let octalStrError = VALIDATE.IsStringInput(octalStr);
-    if (octalStrError)
-      return Promise.reject(`Failed to change permissions: octal string is ${octalStrError}`);
-
-    let pathsError = Error.PathsValidator(paths);
-    if (pathsError)
-      return Promise.reject(`Failed to change permissions: ${pathsError}`);
-
-    if (!executor)
-      return Promise.reject(`Failed to change permissions: Executor is required`);
-
-    let permsObj = PERMISSIONS.Permissions.CreatePermissionsObjectUsingOctalString(octalStr.trim());
-    if (permsObj.error)
-      return Promise.reject(`Failed to change permissions: ${permsObj.error}`);
-
-    return new Promise((resolve, reject) => {
-      let args = [];
-      if (isRecursive)
-        args.push('-R');
-      args.push(octalStr);
-      args = args.concat(paths);
-
-      executor.Execute('chmod', args).then(output => {
-        if (output.stderr) {
-          reject(`Failed to change permissions: ${output.stderr}`);
-          return;
-        }
-        resolve(true);
-      }).catch(error => reject(`Failed to change permissions: ${error}`));
-    });
-  }
-
-  static RemovePermissions(classes, types, paths, isRecursive, executor) { // Example: classes = 'ugo',  types = 'rwx'
-    let classesError = Error.ClassesStringValidator(classes);
-    if (classesError)
-      return Promise.reject(`Failed to remove permissions: ${classesError}`);
-
-    let typesError = Error.TypesStringValidator(types);
-    if (typesError)
-      return Promise.reject(`Failed to remove permissions: ${typesError}`);
-
-    let pathsError = Error.PathsValidator(paths);
-    if (pathsError)
-      return Promise.reject(`Failed to remove permissions: ${pathsError}`);
-
-    if (!executor)
-      return Promise.reject(`Failed to remove permissions: Executor is required`);
-
-    return new Promise((resolve, reject) => {
-      let args = [];
-      if (isRecursive)
-        args.push('-R');
-      args.push(`${classes}-${types}`);
-      args = args.concat(paths);
-
-      executor.Execute('chmod', args).then(output => {
-        if (output.stderr) {
-          reject(`Failed to remove permissions: ${output.stderr}`);
-          return;
-        }
-        resolve(true);
-      }).catch(error => reject(`Failed to remove permissions: ${error}`));
-    });
-  }
-
-  static AddPermissions(classes, types, paths, isRecursive, executor) {
-    let classesError = Error.ClassesStringValidator(classes);
-    if (classesError)
-      return Promise.reject(`Failed to add permissions: ${classesError}`);
-
-    let typesError = Error.TypesStringValidator(types);
-    if (typesError)
-      return Promise.reject(`Failed to add permissions: ${typesError}`);
-
-    let pathsError = Error.PathsValidator(paths);
-    if (pathsError)
-      return Promise.reject(`Failed to add permissions: ${pathsError}`);
-
-    if (!executor)
-      return Promise.reject(`Failed to add permissions: Executor is required`);
-
-    return new Promise((resolve, reject) => {
-      let args = [];
-      if (isRecursive)
-        args.push('-R');
-      args.push(`${classes}+${types}`);
-      args = args.concat(paths);
-
-      executor.Execute('chmod', args).then(output => {
-        if (output.stderr) {
-          reject(`Failed to add permissions: ${output.stderr}`);
-          return;
-        }
-        resolve(true);
-      }).catch(error => reject(`Failed to add permissions: ${error}`));
-    });
-  }
-
-  static SetPermissions(classes, types, paths, isRecursive, executor) {
-    let classesError = Error.ClassesStringValidator(classes);
-    if (classesError)
-      return Promise.reject(`Failed to add permissions: ${classesError}`);
-
-    let typesError = Error.TypesStringValidator(types);
-    if (typesError)
-      return Promise.reject(`Failed to add permissions: ${typesError}`);
-
-    let pathsError = Error.PathsValidator(paths);
-    if (pathsError)
-      return Promise.reject(`Failed to add permissions: ${pathsError}`);
-
-    if (!executor)
-      return Promise.reject(`Failed to add permissions: Executor is required`);
-
-    return new Promise((resolve, reject) => {
-      let args = [];
-      if (isRecursive)
-        args.push('-R');
-
-      let classChars = ['u', 'g', 'o'];
-      let classCharsSet = new Set(classChars);
-      let classesStringSet = new Set(classes.split(''));
-
-      let missingClassChars = Array.from(new Set(classChars.filter(x => !classesStringSet.has(x))));
-      if (missingClassChars.length == 0)
-        args.push(`a=${types}`);
-      else {
-        let typeChars = ['r', 'w', 'x'];
-        args.push(`${classes}=${types},${missingClassChars.join('')}-${typeChars.join('')}`);
+    executor.Execute('chmod', args).then(output => {
+      if (output.stderr) {
+        reject(`Failed to change permissions: ${output.stderr}`);
+        return;
       }
-      args = args.concat(paths);
+      resolve(true);
+    }).catch(error => reject(`Failed to change permissions: ${error}`));
+  });
+}
 
-      executor.Execute('chmod', args).then(output => {
-        if (output.stderr) {
-          reject(`Failed to set permissions: ${output.stderr}`);
-          return;
-        }
-        resolve(true);
-      }).catch(error => reject(`Failed to set permissions: ${error}`));
-    });
-  }
+function UsingOctalString(octalStr, paths, isRecursive, executor) {
+  let octalStrError = VALIDATE.IsStringInput(octalStr);
+  if (octalStrError)
+    return Promise.reject(`Failed to change permissions: octal string is ${octalStrError}`);
 
-  static Manual(args, executor) {
-    return new Promise((resolve, reject) => {
-      let argsError = Error.ArgsValidator(args);
-      if (argsError)
-        return Promise.reject(`Failed to execute chmod: ${argsError}`);
+  let pathsError = PathsValidator(paths);
+  if (pathsError)
+    return Promise.reject(`Failed to change permissions: ${pathsError}`);
 
-      if (!executor)
-        return Promise.reject(`Failed to execute chmod: Executor is required`);
+  if (!executor)
+    return Promise.reject(`Failed to change permissions: Executor is required`);
 
-      executor.Execute('chmod', args).then(output => {
-        if (output.stderr) {
-          reject(`Failed to execute chmod: ${output.stderr} `);
-          return;
-        }
-        resolve(true);
-      }).catch(error => reject(`Failed to execute chmod: ${error} `));
-    });
-  }
+  let permsObj = PERMISSIONS.Permissions.CreatePermissionsObjectUsingOctalString(octalStr.trim());
+  if (permsObj.error)
+    return Promise.reject(`Failed to change permissions: ${permsObj.error}`);
 
-  static ValidClassChars() {
-    return PERMISSIONS.Permissions.ValidClassChars();
-  }
+  return new Promise((resolve, reject) => {
+    let args = [];
+    if (isRecursive)
+      args.push('-R');
+    args.push(octalStr);
+    args = args.concat(paths);
 
-  static ValidTypeChars() {
-    return ['r', 'w', 'x'];
-  }
+    executor.Execute('chmod', args).then(output => {
+      if (output.stderr) {
+        reject(`Failed to change permissions: ${output.stderr}`);
+        return;
+      }
+      resolve(true);
+    }).catch(error => reject(`Failed to change permissions: ${error}`));
+  });
+}
+
+function RemovePermissions(classes, types, paths, isRecursive, executor) { // Example: classes = 'ugo',  types = 'rwx'
+  let classesError = ClassesStringValidator(classes);
+  if (classesError)
+    return Promise.reject(`Failed to remove permissions: ${classesError}`);
+
+  let typesError = TypesStringValidator(types);
+  if (typesError)
+    return Promise.reject(`Failed to remove permissions: ${typesError}`);
+
+  let pathsError = PathsValidator(paths);
+  if (pathsError)
+    return Promise.reject(`Failed to remove permissions: ${pathsError}`);
+
+  if (!executor)
+    return Promise.reject(`Failed to remove permissions: Executor is required`);
+
+  return new Promise((resolve, reject) => {
+    let args = [];
+    if (isRecursive)
+      args.push('-R');
+    args.push(`${classes}-${types}`);
+    args = args.concat(paths);
+
+    executor.Execute('chmod', args).then(output => {
+      if (output.stderr) {
+        reject(`Failed to remove permissions: ${output.stderr}`);
+        return;
+      }
+      resolve(true);
+    }).catch(error => reject(`Failed to remove permissions: ${error}`));
+  });
+}
+
+function AddPermissions(classes, types, paths, isRecursive, executor) {
+  let classesError = ClassesStringValidator(classes);
+  if (classesError)
+    return Promise.reject(`Failed to add permissions: ${classesError}`);
+
+  let typesError = TypesStringValidator(types);
+  if (typesError)
+    return Promise.reject(`Failed to add permissions: ${typesError}`);
+
+  let pathsError = PathsValidator(paths);
+  if (pathsError)
+    return Promise.reject(`Failed to add permissions: ${pathsError}`);
+
+  if (!executor)
+    return Promise.reject(`Failed to add permissions: Executor is required`);
+
+  return new Promise((resolve, reject) => {
+    let args = [];
+    if (isRecursive)
+      args.push('-R');
+    args.push(`${classes}+${types}`);
+    args = args.concat(paths);
+
+    executor.Execute('chmod', args).then(output => {
+      if (output.stderr) {
+        reject(`Failed to add permissions: ${output.stderr}`);
+        return;
+      }
+      resolve(true);
+    }).catch(error => reject(`Failed to add permissions: ${error}`));
+  });
+}
+
+function SetPermissions(classes, types, paths, isRecursive, executor) {
+  let classesError = ClassesStringValidator(classes);
+  if (classesError)
+    return Promise.reject(`Failed to add permissions: ${classesError}`);
+
+  let typesError = TypesStringValidator(types);
+  if (typesError)
+    return Promise.reject(`Failed to add permissions: ${typesError}`);
+
+  let pathsError = PathsValidator(paths);
+  if (pathsError)
+    return Promise.reject(`Failed to add permissions: ${pathsError}`);
+
+  if (!executor)
+    return Promise.reject(`Failed to add permissions: Executor is required`);
+
+  return new Promise((resolve, reject) => {
+    let args = [];
+    if (isRecursive)
+      args.push('-R');
+
+    let classChars = ['u', 'g', 'o'];
+    let classCharsSet = new Set(classChars);
+    let classesStringSet = new Set(classes.split(''));
+
+    let missingClassChars = Array.from(new Set(classChars.filter(x => !classesStringSet.has(x))));
+    if (missingClassChars.length == 0)
+      args.push(`a=${types}`);
+    else {
+      let typeChars = ['r', 'w', 'x'];
+      args.push(`${classes}=${types},${missingClassChars.join('')}-${typeChars.join('')}`);
+    }
+    args = args.concat(paths);
+
+    executor.Execute('chmod', args).then(output => {
+      if (output.stderr) {
+        reject(`Failed to set permissions: ${output.stderr}`);
+        return;
+      }
+      resolve(true);
+    }).catch(error => reject(`Failed to set permissions: ${error}`));
+  });
+}
+
+function Manual(args, executor) {
+  return new Promise((resolve, reject) => {
+    let argsError = ArgsValidator(args);
+    if (argsError)
+      return Promise.reject(`Failed to execute chmod: ${argsError}`);
+
+    if (!executor)
+      return Promise.reject(`Failed to execute chmod: Executor is required`);
+
+    executor.Execute('chmod', args).then(output => {
+      if (output.stderr) {
+        reject(`Failed to execute chmod: ${output.stderr} `);
+        return;
+      }
+      resolve(true);
+    }).catch(error => reject(`Failed to execute chmod: ${error} `));
+  });
+}
+
+function ValidClassChars() {
+  return PERMISSIONS.ValidClassChars();
+}
+
+function ValidTypeChars() {
+  return ['r', 'w', 'x'];
 }
 
 //------------------------------------
 // ERROR
 
-class Error {
-  static ClassesStringValidator(string) {
-    let error = VALIDATE.IsStringInput(string);
-    if (error)
-      return `Classes string is ${error} `;
+function ClassesStringValidator(string) {
+  let error = VALIDATE.IsStringInput(string);
+  if (error)
+    return `Classes string is ${error} `;
 
-    let min = 1;
-    let max = 3;
+  let min = 1;
+  let max = 3;
 
-    // Check length
-    if (string.length < min || string.length > max)
-      return `Classes string must have ${min} to ${max} characters`;
+  // Check length
+  if (string.length < min || string.length > max)
+    return `Classes string must have ${min} to ${max} characters`;
 
-    // Check for invalid chars
-    for (let i = 0; i < string.length; ++i) {
-      let currChar = string.charAt(i);
-      if (!Chmod.ValidClassChars().includes(currChar))
-        return `Classes string contains invalid characters`;
-    }
-
-    return null;
+  // Check for invalid chars
+  for (let i = 0; i < string.length; ++i) {
+    let currChar = string.charAt(i);
+    if (!ValidClassChars().includes(currChar))
+      return `Classes string contains invalid characters`;
   }
 
-  static TypesStringValidator(string) {
-    let error = VALIDATE.IsStringInput(string);
-    if (error)
-      return `Types string is ${error} `;
+  return null;
+}
 
-    let min = 1;
-    let max = 3;
+function TypesStringValidator(string) {
+  let error = VALIDATE.IsStringInput(string);
+  if (error)
+    return `Types string is ${error} `;
 
-    // Check length
-    if (string.length < min || string.length > max)
-      return `Types string must have ${min} to ${max} characters`;
+  let min = 1;
+  let max = 3;
 
-    // Check for invalid chars
-    for (let i = 0; i < string.length; ++i) {
-      let currChar = string.charAt(i);
-      if (!Chmod.ValidTypeChars().includes(currChar))
-        return `Types string contains invalid characters`;
-    }
+  // Check length
+  if (string.length < min || string.length > max)
+    return `Types string must have ${min} to ${max} characters`;
 
-    return null;
+  // Check for invalid chars
+  for (let i = 0; i < string.length; ++i) {
+    let currChar = string.charAt(i);
+    if (!ValidTypeChars().includes(currChar))
+      return `Types string contains invalid characters`;
   }
 
-  static PathsValidator(paths) {
-    let error = VALIDATE.IsArray(paths);
-    if (error)
-      return `Paths are ${error} `;
+  return null;
+}
 
-    for (let i = 0; i < paths.length; ++i) {
-      let currPath = paths[i];
-      let pathIsValid = VALIDATE.IsStringInput(currPath) == null;
+function PathsValidator(paths) {
+  let error = VALIDATE.IsArray(paths);
+  if (error)
+    return `Paths are ${error} `;
 
-      if (!pathIsValid)
-        return `All paths must be valid(non - empty, non - whitespace) strings`;
-    }
+  for (let i = 0; i < paths.length; ++i) {
+    let currPath = paths[i];
+    let pathIsValid = VALIDATE.IsStringInput(currPath) == null;
 
-    return null;
+    if (!pathIsValid)
+      return `All paths must be valid(non - empty, non - whitespace) strings`;
   }
 
-  static ArgsValidator(args) {
-    let error = VALIDATE.IsArray(args);
-    if (error)
-      return `arguments are ${error} `;
+  return null;
+}
 
-    for (let i = 0; i < args.length; ++i) {
-      let currArg = args[i];
-      let argIsValidString = VALIDATE.IsStringInput(currArg) == null;
-      let argIsValidNumber = !isNaN(currArg);
+function ArgsValidator(args) {
+  let error = VALIDATE.IsArray(args);
+  if (error)
+    return `arguments are ${error} `;
 
-      if (!argIsValidString && !argIsValidNumber)
-        return `arg elements must be string or number type`;
-    }
+  for (let i = 0; i < args.length; ++i) {
+    let currArg = args[i];
+    let argIsValidString = VALIDATE.IsStringInput(currArg) == null;
+    let argIsValidNumber = !isNaN(currArg);
 
-    return null;
+    if (!argIsValidString && !argIsValidNumber)
+      return `arg elements must be string or number type`;
   }
+
+  return null;
 }
 
 //------------------------------------
 // EXPORTS
 
-exports.Chmod = Chmod;
+exports.UsingPermString = UsingPermString;
+exports.UsingOctalString = UsingOctalString;
+exports.RemovePermissions = RemovePermissions;
+exports.AddPermissions = AddPermissions;
+exports.SetPermissions = SetPermissions;
+exports.Manual = Manual;
