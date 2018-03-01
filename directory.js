02a65ffba23372ac215f62489603fe60da48fceb
Jesus Isaias Arevalo <isa90201@gmail.com>
2018-03-01 09:11:52 -0800

    Updated code to reflect changes made after moving functions out of their class.

--- directory.js
+++ directory.js
@@ -1,49 +1,49 @@
-let REMOVE = require('./remove.js').Remove;
-let MKDIR = require('./mkdir.js').Mkdir;
+let REMOVE = require('./remove.js');
+let MKDIR = require('./mkdir.js');
 let PATH = require('./path.js');
-let DISKUSAGE = require('./diskusage.js').DiskUsage;
+let DISKUSAGE = require('./diskusage.js');
 let VALIDATE = require('./validate.js');
 
 //-----------------------------------------
 // DIRECTORY
 
-class Directory {
-  static Remove(path, executor) {
-    return REMOVE.Directories([path], executor);
-  }
+function Remove(path, executor) {
+  return REMOVE.Directories([path], executor);
+}
 
-  static Create(path, executor) {
-    let pathError = VALIDATE.IsStringInput(path);
-    if (pathError)
-      return Promise.reject(`Failed to create directory: ${pathError}`);
+function Create(path, executor) {
+  let pathError = VALIDATE.IsStringInput(path);
+  if (pathError)
+    return Promise.reject(`Failed to create directory: ${pathError}`);
 
-    if (!executor)
-      return Promise.reject(`Failed to create directory: Executor is required`);
+  if (!executor)
+    return Promise.reject(`Failed to create directory: Executor is required`);
 
-    return new Promise((resolve, reject) => {
-      MKDIR.Mkdirp(path, executor).then(success => {
-        resolve(true);
-      }).catch(error => reject(`Failed tp create directory: ${error}`));
-    });
-  }
+  return new Promise((resolve, reject) => {
+    MKDIR.Mkdirp(path, executor).then(success => {
+      resolve(true);
+    }).catch(error => reject(`Failed tp create directory: ${error}`));
+  });
+}
 
-  static Size(path, executor) {
-    let pathError = VALIDATE.IsStringInput(path);
-    if (pathError)
-      return Promise.reject(`Failed to get directory size: ${pathError}`);
+function Size(path, executor) {
+  let pathError = VALIDATE.IsStringInput(path);
+  if (pathError)
+    return Promise.reject(`Failed to get directory size: ${pathError}`);
 
-    if (!executor)
-      return Promise.reject(`Failed to get directory size: Executor is required`);
+  if (!executor)
+    return Promise.reject(`Failed to get directory size: Executor is required`);
 
-    return new Promise((resolve, reject) => {
-      DISKUSAGE.DirSize(path, executor).then(size => {
-        resolve(size);
-      }).catch(reject);
-    });
-  }
+  return new Promise((resolve, reject) => {
+    DISKUSAGE.DirSize(path, executor).then(size => {
+      resolve(size);
+    }).catch(reject);
+  });
 }
 
 //--------------------------------------
 // EXPORTS
 
-exports.Directory = Directory;
+exports.Remove = Remove;
+exports.Create = Create;
+exports.Size = Size;
