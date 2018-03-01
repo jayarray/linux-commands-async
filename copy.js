efc7bef7896052a05457b3d79b55f589032d4a8b
Jesus Isaias Arevalo <isa90201@gmail.com>
2018-03-01 09:09:46 -0800

    Updated code to reflect changes made after moving functions out fo their classes.

--- copy.js
+++ copy.js
@@ -3,55 +3,54 @@ let VALIDATE = require('./validate.js');
 //-------------------------------------------------
 // COPY (cp)
 
-class Copy {
-  static File(src, dest, executor) {
-    let srcError = VALIDATE.IsStringInput(src);
-    if (srcError)
-      return Promise.reject(`Failed to copy file: Source is ${srcError}`);
+function File(src, dest, executor) {
+  let srcError = VALIDATE.IsStringInput(src);
+  if (srcError)
+    return Promise.reject(`Failed to copy file: Source is ${srcError}`);
 
-    let destError = VALIDATE.IsStringInput(dest);
-    if (destError)
-      return Promise.reject(`Failed to copy file: Destination is ${destError}`);
+  let destError = VALIDATE.IsStringInput(dest);
+  if (destError)
+    return Promise.reject(`Failed to copy file: Destination is ${destError}`);
 
-    if (!executor)
-      return Promise.reject(`Failed to copy file: Executor is required`);
+  if (!executor)
+    return Promise.reject(`Failed to copy file: Executor is required`);
 
-    return new Promise((resolve, reject) => {
-      executor.Execute('cp', [src, dest]).then(output => {
-        if (output.stderr) {
-          reject(`Failed to copy file: ${output.stderr}`);
-          return;
-        }
-        resolve(true);
-      }).catch(error => reject(`Failed to copy file: ${error}`));
-    });
-  }
+  return new Promise((resolve, reject) => {
+    executor.Execute('cp', [src, dest]).then(output => {
+      if (output.stderr) {
+        reject(`Failed to copy file: ${output.stderr}`);
+        return;
+      }
+      resolve(true);
+    }).catch(error => reject(`Failed to copy file: ${error}`));
+  });
+}
 
-  static Direcory(src, dest, executor) {
-    let srcError = VALIDATE.IsStringInput(src);
-    if (srcError)
-      return Promise.reject(`Failed to copy directory: Source is ${srcError}`);
+function Direcory(src, dest, executor) {
+  let srcError = VALIDATE.IsStringInput(src);
+  if (srcError)
+    return Promise.reject(`Failed to copy directory: Source is ${srcError}`);
 
-    let destError = VALIDATE.IsStringInput(dest);
-    if (destError)
-      return Promise.reject(`Failed to copy directory: Destination is ${destError}`);
+  let destError = VALIDATE.IsStringInput(dest);
+  if (destError)
+    return Promise.reject(`Failed to copy directory: Destination is ${destError}`);
 
-    if (!executor)
-      return Promise.reject(`Failed to copy directory: Executor is required`);
+  if (!executor)
+    return Promise.reject(`Failed to copy directory: Executor is required`);
 
-    return new Promise((resolve, reject) => {
-      executor.Execute('cp', ['-R', src, dest]).then(output => {
-        if (output.stderr) {
-          reject(`Failed to copy directory: ${output.stderr}`);
-          return;
-        }
-        resolve(true);
-      }).catch(error => reject(`Failed to copy directory: ${error}`));
-    });
-  }
+  return new Promise((resolve, reject) => {
+    executor.Execute('cp', ['-R', src, dest]).then(output => {
+      if (output.stderr) {
+        reject(`Failed to copy directory: ${output.stderr}`);
+        return;
+      }
+      resolve(true);
+    }).catch(error => reject(`Failed to copy directory: ${error}`));
+  });
 }
 
 //----------------------------------------
 // EXPORTS
 
-exports.Copy = Copy;
+exports.File = File;
+exports.Direcory = Direcory;
