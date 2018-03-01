a4c732ce961eeb9299c9ea58c333fec0345a8bc7
Jesus Isaias Arevalo <isa90201@gmail.com>
2018-03-01 09:28:33 -0800

    Upodated code tor eflect changes made after moving functions out of their class.

--- mkdir.js
+++ mkdir.js
@@ -1,51 +1,28 @@
-import { exec } from 'child_process';
-
 let VALIDATE = require('./validate.js');
 
 //------------------------------------------------------
 // MKDIR (mkdir)
 
-class Mkdir {
-  static Mkdir(path, executor) {
-    let pathError = VALIDATE.IsStringInput(path);
-    if (pathError)
-      return Promise.reject(`Failed to make directory: ${pathError}`);
+function MakeDirectory(path, executor) {
+  let pathError = VALIDATE.IsStringInput(path);
+  if (pathError)
+    return Promise.reject(`Failed to make directory: ${pathError}`);
 
-    if (!executor)
-      return Promise.reject(`Failed to make directory: Executor is required`);
+  if (!executor)
+    return Promise.reject(`Failed to make directory: Executor is required`);
 
-    return new Promise((resolve, reject) => {
-      executor.Execute('mkdir', [path]).then(output => {
-        if (output.stderr) {
-          reject(`Failed to make directory: ${output.stderr}`);
-          return;
-        }
-        resolve(true);
-      }).catch(error => reject(`Failed to make directory: ${error}`));
-    });
-  }
-
-  static Mkdirp(path, executor) {
-    let pathError = VALIDATE.IsStringInput(path);
-    if (pathError)
-      return Promise.reject(`Failed to make directory path: ${pathError}`);
-
-    if (!executor)
-      return Promise.reject(`Failed to make directory path:Executor is required`);
-
-    return new Promise((resolve, reject) => {
-      executor.Execute('mkdir', ['-p', path]).then(output => {
-        if (output.stderr) {
-          reject(`Failed to make directory path: ${output.stderr}`);
-          return;
-        }
-        resolve(true);
-      }).catch(error => reject(`Failed to make directory path: ${error}`));
-    });
-  }
+  return new Promise((resolve, reject) => {
+    executor.Execute('mkdir', ['-p', path]).then(output => {
+      if (output.stderr) {
+        reject(`Failed to make directory: ${output.stderr}`);
+        return;
+      }
+      resolve(true);
+    }).catch(error => reject(`Failed to make directory: ${error}`));
+  });
 }
 
 //-------------------------------------
 // EXPORTS
 
-exports.Mkdir = Mkdir;
+exports.MakeDirectory = MakeDirectory;
