9edbd044c7a9b311137ecd230b1419508f0932ba
Jesus Isaias Arevalo <isa90201@gmail.com>
2018-03-01 09:01:59 -0800

    Updated code to reflect changes made after moving functions out of their classes.

--- chown.js
+++ chown.js
@@ -2,168 +2,168 @@ let VALIDATE = require('./validate.js');
 
 //-----------------------------------------------
 // CHOWN
-class Chown {
-  static ChangeOwner(paths, newOwnerId, isRecursive, executor) { // newOwner can be string or integer
-    let pathsError = Error.PathsValidator(paths);
-    if (pathsError)
-      return Promise.reject(`Failed to change owner: ${pathsError}`);
 
-    let idError = Error.newIdValidator(newOwnerId);
-    if (idError)
-      return Promise.reject(`Failed to change owner: new owner id ${idError}`);
+function ChangeOwner(paths, newOwnerId, isRecursive, executor) { // newOwner can be string or integer
+  let pathsError = PathsValidator(paths);
+  if (pathsError)
+    return Promise.reject(`Failed to change owner: ${pathsError}`);
 
-    if (!executor)
-      return Promise.reject(`Failed to change owner: Executor is required`);
+  let idError = NewIdValidator(newOwnerId);
+  if (idError)
+    return Promise.reject(`Failed to change owner: new owner id ${idError}`);
 
-    return new Promise((resolve, reject) => {
-      let args = [];
-      if (isRecursive)
-        args.push('-R');
-      args.push(newOwnerId);
-      args = args.concat(paths);
+  if (!executor)
+    return Promise.reject(`Failed to change owner: Executor is required`);
 
-      executor.Execute('chown', args).then(output => {
-        if (output.stderr) {
-          reject(`Failed to change owner: ${output.stderr}`);
-          return;
-        }
-        resolve(true);
-      }).catch(error => reject(`Failed to change owner: ${error}`));
-    });
-  }
+  return new Promise((resolve, reject) => {
+    let args = [];
+    if (isRecursive)
+      args.push('-R');
+    args.push(newOwnerId);
+    args = args.concat(paths);
 
-  static ChangeGroup(paths, newGroupId, isRecursive, executor) { // newOwner can be string or integer
-    let pathsError = Error.PathsValidator(paths);
-    if (pathsError)
-      return Promise.reject(`Failed to change group: ${pathsError}`);
+    executor.Execute('chown', args).then(output => {
+      if (output.stderr) {
+        reject(`Failed to change owner: ${output.stderr}`);
+        return;
+      }
+      resolve(true);
+    }).catch(error => reject(`Failed to change owner: ${error}`));
+  });
+}
 
-    if (!executor)
-      return Promise.reject(`Failed to change group: Executor is required`);
+function ChangeGroup(paths, newGroupId, isRecursive, executor) { // newOwner can be string or integer
+  let pathsError = PathsValidator(paths);
+  if (pathsError)
+    return Promise.reject(`Failed to change group: ${pathsError}`);
 
-    let idError = Error.newIdValidator(newGroupId);
-    if (idError)
-      return Promise.reject(`Failed to change group: new group id ${idError}`);
+  if (!executor)
+    return Promise.reject(`Failed to change group: Executor is required`);
 
-    return new Promise((resolve, reject) => {
-      let args = [];
-      if (isRecursive)
-        args.push('-R');
-      args.push(`:${newGroupId}`);
-      args = args.concat(paths);
+  let idError = NewIdValidator(newGroupId);
+  if (idError)
+    return Promise.reject(`Failed to change group: new group id ${idError}`);
 
-      executor.Execute('chown', args).then(output => {
-        if (output.stderr) {
-          reject(`Failed to change group: ${output.stderr}`);
-          return;
-        }
-        resolve(true);
-      }).catch(error => reject(`Failed to change group: ${error}`));
-    });
-  }
+  return new Promise((resolve, reject) => {
+    let args = [];
+    if (isRecursive)
+      args.push('-R');
+    args.push(`:${newGroupId}`);
+    args = args.concat(paths);
 
-  static ChangeOwnerAndGroup(paths, newOwnerId, newGroupId, isRecursive, executor) { // newOwner can be string or integer
-    let pathsError = Error.PathsValidator(paths);
-    if (pathsError)
-      return Promise.reject(`Failed to change group: ${pathsError}`);
+    executor.Execute('chown', args).then(output => {
+      if (output.stderr) {
+        reject(`Failed to change group: ${output.stderr}`);
+        return;
+      }
+      resolve(true);
+    }).catch(error => reject(`Failed to change group: ${error}`));
+  });
+}
 
-    if (!executor)
-      return Promise.reject(`Failed to change owner and group: Executor is required`);
+function ChangeOwnerAndGroup(paths, newOwnerId, newGroupId, isRecursive, executor) { // newOwner can be string or integer
+  let pathsError = PathsValidator(paths);
+  if (pathsError)
+    return Promise.reject(`Failed to change group: ${pathsError}`);
 
-    let idError = Error.newIdValidator(newOwnerId);
-    if (idError)
-      return Promise.reject(`Failed to change owner and group: new owner id ${idError}`);
+  if (!executor)
+    return Promise.reject(`Failed to change owner and group: Executor is required`);
 
-    idError = Error.newIdValidator(newGroupId);
-    if (idError)
-      return Promise.reject(`Failed to change owner and group: new group id ${idError}`);
+  let idError = NewIdValidator(newOwnerId);
+  if (idError)
+    return Promise.reject(`Failed to change owner and group: new owner id ${idError}`);
 
-    return new Promise((resolve, reject) => {
-      let args = [];
-      if (isRecursive)
-        args.push('-R');
-      args.push(`${newOwnerId}:${newGroupId}`);
-      args = args.concat(paths);
+  idError = NewIdValidator(newGroupId);
+  if (idError)
+    return Promise.reject(`Failed to change owner and group: new group id ${idError}`);
 
-      executor.Execute('chown', args).then(output => {
-        if (output.stderr) {
-          reject(`Failed to change owner and group: ${output.stderr}`);
-          return;
-        }
-        resolve(true);
-      }).catch(error => reject(`Failed to change owner and group: ${error}`));
-    });
-  }
+  return new Promise((resolve, reject) => {
+    let args = [];
+    if (isRecursive)
+      args.push('-R');
+    args.push(`${newOwnerId}:${newGroupId}`);
+    args = args.concat(paths);
 
-  static Manual(args, executor) { // newOwner can be string or integer
-    let argsError = Error.ArgsValidator(args);
-    if (argsError)
-      return Promise.reject(`Failed to execute chown: ${argsError}`);
+    executor.Execute('chown', args).then(output => {
+      if (output.stderr) {
+        reject(`Failed to change owner and group: ${output.stderr}`);
+        return;
+      }
+      resolve(true);
+    }).catch(error => reject(`Failed to change owner and group: ${error}`));
+  });
+}
 
-    if (!executor)
-      return Promise.reject(`Failed to execute chown: Executor is required`);
+function Manual(args, executor) { // newOwner can be string or integer
+  let argsError = ArgsValidator(args);
+  if (argsError)
+    return Promise.reject(`Failed to execute chown: ${argsError}`);
 
-    return new Promise((resolve, reject) => {
-      executor.Execute('chown', args).then(output => {
-        if (output.stderr) {
-          reject(`Failed to execute chown: ${output.stderr}`);
-          return;
-        }
-        resolve(true);
-      }).catch(error => reject(`Failed to execute chown: ${error}`));
-    });
-  }
+  if (!executor)
+    return Promise.reject(`Failed to execute chown: Executor is required`);
+
+  return new Promise((resolve, reject) => {
+    executor.Execute('chown', args).then(output => {
+      if (output.stderr) {
+        reject(`Failed to execute chown: ${output.stderr}`);
+        return;
+      }
+      resolve(true);
+    }).catch(error => reject(`Failed to execute chown: ${error}`));
+  });
 }
 
 //-----------------------------------
 // ERROR
 
-class Error {
-  static newIdValidator(id) {
-    let error = ERROR.NullOrUndefined(id);
-    if (error)
-      return `is ${error}`;
+function NewIdValidator(id) {
+  let error = ERROR.NullOrUndefined(id);
+  if (error)
+    return `is ${error}`;
 
-    if (typeof id != 'string' && !Number.isInteger(id))
-      return `must be a string or integer`;
+  if (typeof id != 'string' && !Number.isInteger(id))
+    return `must be a string or integer`;
 
-    return null;
+  return null;
+}
+
+function PathsValidator(paths) {
+  let error = ERROR.ArrayValidator(paths);
+  if (error)
+    return `Paths are ${error}`;
+
+  for (let i = 0; i < paths.length; ++i) {
+    let currPath = paths[i];
+    let pathIsValid = ERROR.StringValidator(currPath) == null;
+
+    if (!pathIsValid)
+      return `All paths must be string type`;
   }
 
-  static PathsValidator(paths) {
-    let error = ERROR.ArrayValidator(paths);
-    if (error)
-      return `Paths are ${error}`;
+  return null;
+}
 
-    for (let i = 0; i < paths.length; ++i) {
-      let currPath = paths[i];
-      let pathIsValid = ERROR.StringValidator(currPath) == null;
+function ArgsValidator(args) {
+  let error = ERROR.ArrayValidator(args);
+  if (error)
+    return `arguments are ${error}`;
 
-      if (!pathIsValid)
-        return `All paths must be string type`;
-    }
+  for (let i = 0; i < args.length; ++i) {
+    let currArg = args[i];
+    let argIsValidString = ERROR.StringValidator(currArg) == null;
+    let argIsValidNumber = !isNaN(currArg);
 
-    return null;
+    if (!argIsValidString && !argIsValidNumber)
+      return `arg elements must be string or number type`;
   }
 
-  static ArgsValidator(args) {
-    let error = ERROR.ArrayValidator(args);
-    if (error)
-      return `arguments are ${error}`;
-
-    for (let i = 0; i < args.length; ++i) {
-      let currArg = args[i];
-      let argIsValidString = ERROR.StringValidator(currArg) == null;
-      let argIsValidNumber = !isNaN(currArg);
-
-      if (!argIsValidString && !argIsValidNumber)
-        return `arg elements must be string or number type`;
-    }
-
-    return null;
-  }
+  return null;
 }
 
 //-----------------------------------
 // EXPORTS
 
-exports.Chown = Chown;
+exports.ChangeOwner = ChangeOwner;
+exports.ChangeGroup = ChangeGroup;
+exports.ChangeOwnerAndGroup = ChangeOwnerAndGroup;
+exports.Manual = Manual;
