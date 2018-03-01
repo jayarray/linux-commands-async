9ddbfb4b3648546a68737cc84e390862c9e79faa
Jesus Isaias Arevalo <isa90201@gmail.com>
2018-03-01 09:16:26 -0800

    Updated code tor eflect changes made after moving functions out of their class.

--- file.js
+++ file.js
@@ -1,96 +1,99 @@
 let VALIDATE = require('./validate.js');
-let REMOVE = require('./remove.js').Remove;
-let CHMOD = require('./chmod.js').Chmod;
-let PATH = require('./path.js').Path;
+let REMOVE = require('./remove.js');
+let CHMOD = require('./chmod.js');
+let PATH = require('./path.js');
 
 //---------------------------------------------------
 // FILE
-class File {
-  static Remove(path, executor) {
-    return REMOVE.Files([path], executor);
-  }
 
-  static Create(path, text, executor) {
-    let pathError = VALIDATE.IsStringInput(path);
-    if (pathError)
-      return Promise.reject(`Failed to create file: path is ${pathError}`);
+function Remove(path, executor) {
+  return REMOVE.Files([path], executor);
+}
 
-    let textError = VALIDATE.IsStringInput(text);
-    if (textError)
-      return Promise.reject(`Failed to create file: text is ${textError}`);
+function Create(path, text, executor) {
+  let pathError = VALIDATE.IsStringInput(path);
+  if (pathError)
+    return Promise.reject(`Failed to create file: path is ${pathError}`);
 
-    if (!executor)
-      return Promise.reject(`Failed to create file: Executor is required`);
+  let textError = VALIDATE.IsStringInput(text);
+  if (textError)
+    return Promise.reject(`Failed to create file: text is ${textError}`);
 
-    return new Promise((resolve, reject) => {
-      executor.Execute(`echo "${text}" > ${path}`, []).then(output => {
-        if (output.stderr) {
-          reject(`Failed to create file: ${output.stderr}`);
-          return;
-        }
+  if (!executor)
+    return Promise.reject(`Failed to create file: Executor is required`);
+
+  return new Promise((resolve, reject) => {
+    executor.Execute(`echo "${text}" > ${path}`, []).then(output => {
+      if (output.stderr) {
+        reject(`Failed to create file: ${output.stderr}`);
+        return;
+      }
+      resolve(true);
+    }).catch(error => reject(`Failed to create file: ${error}`));
+  });
+}
+
+function MakeExecutable(path, executor) {
+  let pathError = VALIDATE.IsStringInput(path);
+  if (pathError)
+    return Promise.reject(`Failed to make file executable: path is ${pathError}`);
+
+  if (!executor)
+    return Promise.reject(`Failed to make file executable: Executor is required`);
+
+  return new Promise((resolve, reject) => {
+    PATH.IsFile(path, executor).then(isFile => {
+      if (!isFile) {
+        reject(`Failed to make file executable: path is not a file: ${path}`);
+        return;
+      }
+
+      CHMOD.AddPermissions('ugo', 'x', [path], false, executor).then(success => {
         resolve(true);
-      }).catch(error => reject(`Failed to create file: ${error}`));
-    });
-  }
-
-  static MakeExecutable(path, executor) {
-    let pathError = VALIDATE.IsStringInput(path);
-    if (pathError)
-      return Promise.reject(`Failed to make file executable: path is ${pathError}`);
-
-    if (!executor)
-      return Promise.reject(`Failed to make file executable: Executor is required`);
-
-    return new Promise((resolve, reject) => {
-      PATH.IsFile(path, executor).then(isFile => {
-        if (!isFile) {
-          reject(`Failed to make file executable: path is not a file: ${path}`);
-          return;
-        }
-
-        CHMOD.AddPermissions('ugo', 'x', [path], false, executor).then(success => {
-          resolve(true);
-        }).catch(error => reject(`Failed to make file executable: ${error}`));
       }).catch(error => reject(`Failed to make file executable: ${error}`));
-    });
-  }
+    }).catch(error => reject(`Failed to make file executable: ${error}`));
+  });
+}
 
-  static Read(path, executor) {
-    let pathError = VALIDATE.IsStringInput(path);
-    if (pathError)
-      return Promise.reject(`Failed to read file: path is ${pathError}`);
+function Read(path, executor) {
+  let pathError = VALIDATE.IsStringInput(path);
+  if (pathError)
+    return Promise.reject(`Failed to read file: path is ${pathError}`);
 
-    if (!executor)
-      return Promise.reject(`Failed to read file: Executor is required`);
+  if (!executor)
+    return Promise.reject(`Failed to read file: Executor is required`);
 
-    return new Promise((resolve, reject) => {
-      PATH.IsFile(path, executor).then(isFile => {
-        if (!isFile) {
-          reject(`Failed to read file: path is not a file: ${path}`);
+  return new Promise((resolve, reject) => {
+    PATH.IsFile(path, executor).then(isFile => {
+      if (!isFile) {
+        reject(`Failed to read file: path is not a file: ${path}`);
+        return;
+      }
+
+      executor.Execute('cat', [path]).then(output => {
+        if (output.stderr) {
+          reject(`Failed to read file: ${output.stderr}`);
           return;
         }
-
-        executor.Execute('cat', [path]).then(output => {
-          if (output.stderr) {
-            reject(`Failed to read file: ${output.stderr}`);
-            return;
-          }
-          resolve(output.stdout);
-        }).catch(error => reject(`Failed to read file: ${error}`));
+        resolve(output.stdout);
       }).catch(error => reject(`Failed to read file: ${error}`));
-    });
-  }
+    }).catch(error => reject(`Failed to read file: ${error}`));
+  });
+}
 
-  static ReadLines(path, executor) {
-    return new Promise((resolve, reject) => {
-      File.Read(path, executor).then(string => {
-        resolve(string.split('\n'));
-      }).catch(error => reject(`Failed to read lines: ${error}`));
-    });
-  }
+function ReadLines(path, executor) {
+  return new Promise((resolve, reject) => {
+    Read(path, executor).then(string => {
+      resolve(string.split('\n'));
+    }).catch(error => reject(`Failed to read lines: ${error}`));
+  });
 }
 
 //-------------------------------
 // EXPORTS
 
-exports.File = File;
+exports.Remove = Remove;
+exports.Create = Create;
+exports.MakeExecutable = MakeExecutable;
+exports.Read = Read;
+exports.ReadLines = ReadLines;
