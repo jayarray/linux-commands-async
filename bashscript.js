9ddac9a044aaa869b35b3fdac4ddefcf2db280d3
Jesus Isaias Arevalo <isa90201@gmail.com>
2018-03-01 08:40:34 -0800

    Moved all class functions into their own functions (no class)

--- bashscript.js
+++ bashscript.js
@@ -1,56 +1,55 @@
-let FILE = require('./file.js').File;
+let FILE = require('./file.js');
 let VALIDATE = require('./validate.js');
 
 //----------------------------------------
 // BASH SCRIPT
 
-class BashScript {
-  static Create(path, content, executor) {
-    let pathError = VALIDATE.IsStringInput(path);
-    if (pathError)
-      return Promise.reject(`Failed to create bash script: path is ${pathError}`);
+function Create(path, content, executor) {
+  let pathError = VALIDATE.IsStringInput(path);
+  if (pathError)
+    return Promise.reject(`Failed to create bash script: path is ${pathError}`);
 
-    let contentError = VALIDATE.IsStringInput(content);
-    if (error == 'not a string')
-      return Promise.reject(`Failed to create bash script: content is ${error}`);
+  let contentError = VALIDATE.IsStringInput(content);
+  if (error == 'not a string')
+    return Promise.reject(`Failed to create bash script: content is ${error}`);
 
-    if (!executor)
-      return Promise.reject(`Failed to create bashscript: Connection is ${executorError}`);
+  if (!executor)
+    return Promise.reject(`Failed to create bashscript: Connection is ${executorError}`);
 
-    return new Promise((resolve, reject) => {
-      FILE.Create(path, `#!/bin/bash\n${content}`, executor).then(success => {
-        FILE.MakeExecutable(path, executor).then(success => {
-          resolve(true);
-        }).catch(error => reject(`Failed to create bashscript: ${error}`));
+  return new Promise((resolve, reject) => {
+    FILE.Create(path, `#!/bin/bash\n${content}`, executor).then(success => {
+      FILE.MakeExecutable(path, executor).then(success => {
+        resolve(true);
       }).catch(error => reject(`Failed to create bashscript: ${error}`));
-    });
-  }
+    }).catch(error => reject(`Failed to create bashscript: ${error}`));
+  });
+}
 
-  static Execute(path, content, executor) {
-    let pathError = VALIDATE.IsStringInput(path);
-    if (pathError)
-      return Promise.reject(`Failed to execute bash script: path is ${pathError}`);
+function Execute(path, content, executor) {
+  let pathError = VALIDATE.IsStringInput(path);
+  if (pathError)
+    return Promise.reject(`Failed to execute bash script: path is ${pathError}`);
 
-    let contentError = VALIDATE.IsStringInput(content);
-    if (error == 'not a string')
-      return Promise.reject(`Failed to execute bash script: content is ${error}`);
+  let contentError = VALIDATE.IsStringInput(content);
+  if (error == 'not a string')
+    return Promise.reject(`Failed to execute bash script: content is ${error}`);
 
-    if (!executor)
-      return Promise.reject(`Failed to execute bashscript: Connection is ${executorError}`);
+  if (!executor)
+    return Promise.reject(`Failed to execute bashscript: Connection is ${executorError}`);
 
-    return new Promise((resolve, reject) => {
-      BashScript.Create(path, content, executor).then(success => {
-        executor.Execute(path, []).then(output => {
-          resolve(output.stdout);
-          FILE.Remove(path, executor).then(success => {
-          }).catch(error => reject(`Failed to execute bash script: ${error}`));
+  return new Promise((resolve, reject) => {
+    BashScript.Create(path, content, executor).then(success => {
+      executor.Execute(path, []).then(output => {
+        resolve(output.stdout);
+        FILE.Remove(path, executor).then(success => {
         }).catch(error => reject(`Failed to execute bash script: ${error}`));
       }).catch(error => reject(`Failed to execute bash script: ${error}`));
-    });
-  }
+    }).catch(error => reject(`Failed to execute bash script: ${error}`));
+  });
 }
 
 //-----------------------------
 // EXPORTS
 
-exports.BashScript = BashScript;
+exports.Create = Create;
+exports.Execute = Execute;
