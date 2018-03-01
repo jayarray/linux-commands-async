eecf1b6b218bcdba81ea28be7fc1744fb3aaafdc
Jesus Isaias Arevalo <isa90201@gmail.com>
2018-03-01 09:48:15 -0800

    Updated code tor eflect changes made after moving functions out of their class.

--- remove.js
+++ remove.js
@@ -3,47 +3,46 @@ let VALIDATE = require('./validate.js');
 //-------------------------------------------------
 // REMOVE (rm)
 
-class Remove {
-  static Files(paths, executor) {
-    let pathsError = VALIDATE.IsArray(paths);
-    if (pathsError)
-      return Promise.reject(`Failed to remove files: paths are ${pathsError}`);
+function Files(paths, executor) {
+  let pathsError = VALIDATE.IsArray(paths);
+  if (pathsError)
+    return Promise.reject(`Failed to remove files: paths are ${pathsError}`);
 
-    if (!executor)
-      return Promise.reject(`Failed to remove files: Executor is required`);
+  if (!executor)
+    return Promise.reject(`Failed to remove files: Executor is required`);
 
-    return new Promise((resolve, reject) => {
-      executor.Execute('rm', ['-f'].concat(paths)).then(output => {
-        if (output.stderr) {
-          reject(`Failed to remove files: ${output.stderr}`);
-          return;
-        }
-        resolve(true);
-      }).catch(error => reject(`Failed to remove files: ${error}`));
-    });
-  }
+  return new Promise((resolve, reject) => {
+    executor.Execute('rm', ['-f'].concat(paths)).then(output => {
+      if (output.stderr) {
+        reject(`Failed to remove files: ${output.stderr}`);
+        return;
+      }
+      resolve(true);
+    }).catch(error => reject(`Failed to remove files: ${error}`));
+  });
+}
 
-  static Directories(paths, executor) {
-    let pathsError = VALIDATE.IsArray(paths);
-    if (pathsError)
-      return Promise.reject(`Failed to remove directories: paths are ${pathsError}`);
+function Directories(paths, executor) {
+  let pathsError = VALIDATE.IsArray(paths);
+  if (pathsError)
+    return Promise.reject(`Failed to remove directories: paths are ${pathsError}`);
 
-    if (!executor)
-      return Promise.reject(`Failed to remove directories: Executor is required`);
+  if (!executor)
+    return Promise.reject(`Failed to remove directories: Executor is required`);
 
-    return new Promise((resolve, reject) => {
-      executor.Execute('rm', ['-fR'].concat(paths)).then(output => {
-        if (output.stderr) {
-          reject(`Failed to remove directory: ${output.stderr}`);
-          return;
-        }
-        resolve(true);
-      }).catch(error => reject(`Failed to remove directories: ${error}`));
-    });
-  }
+  return new Promise((resolve, reject) => {
+    executor.Execute('rm', ['-fR'].concat(paths)).then(output => {
+      if (output.stderr) {
+        reject(`Failed to remove directory: ${output.stderr}`);
+        return;
+      }
+      resolve(true);
+    }).catch(error => reject(`Failed to remove directories: ${error}`));
+  });
 }
 
 //------------------------------
 // EXPORTS
 
-exports.Remove = Remove;
+exports.Files = Files;
+exports.Directories = Directories;
