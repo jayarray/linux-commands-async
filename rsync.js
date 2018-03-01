e78afe939b0a99d1106f38ed1c20ff0ea85f16cb
Jesus Isaias Arevalo <isa90201@gmail.com>
2018-03-01 09:51:19 -0800

    Updated code to reflect changes made after moving fucntions out of their class.

--- rsync.js
+++ rsync.js
@@ -23,140 +23,142 @@ function argsValidator(args) {
 //------------------------------------------------
 // RSYNC
 
-class Rsync {
-  static Rsync(user, host, src, dest, executor) {
-    let userError = VALIDATE.IsStringInput(user);
-    if (userError)
-      return Promise.reject(`Failed to execute rsync: user is ${userError}`);
+function Rsync(user, host, src, dest, executor) {
+  let userError = VALIDATE.IsStringInput(user);
+  if (userError)
+    return Promise.reject(`Failed to execute rsync: user is ${userError}`);
 
-    let hostError = VALIDATE.IsStringInput(host);
-    if (hostError)
-      return Promise.reject(`Failed to execute rsync: host is ${hostError}`);
+  let hostError = VALIDATE.IsStringInput(host);
+  if (hostError)
+    return Promise.reject(`Failed to execute rsync: host is ${hostError}`);
 
-    let srcError = VALIDATE.IsStringInput(src);
-    if (srcError)
-      return Promise.reject(`Failed to execute rsync: source is ${srcError}`);
+  let srcError = VALIDATE.IsStringInput(src);
+  if (srcError)
+    return Promise.reject(`Failed to execute rsync: source is ${srcError}`);
 
-    let destError = VALIDATE.IsStringInput(dest);
-    if (destError)
-      return Promise.reject(`Failed to execute rsync: destination is ${destError}`);
+  let destError = VALIDATE.IsStringInput(dest);
+  if (destError)
+    return Promise.reject(`Failed to execute rsync: destination is ${destError}`);
 
-    if (!executor)
-      return Promise.reject(`Failed to execute rsync: Executor is required`);
+  if (!executor)
+    return Promise.reject(`Failed to execute rsync: Executor is required`);
 
-    return new Promise((resolve, reject) => {
-      executor.Execute('rsync', ['-a', src, `${user}@${host}:${dest}`]).then(output => {
-        if (output.stderr) {
-          reject(`Failed to execute rsync: ${output.stderr}`);
-          return;
-        }
-        resolve(output.stdout);
-      }).catch(error => reject(`Failed to execute rsync: ${error}`));
-    });
-  }
+  return new Promise((resolve, reject) => {
+    executor.Execute('rsync', ['-a', src, `${user}@${host}:${dest}`]).then(output => {
+      if (output.stderr) {
+        reject(`Failed to execute rsync: ${output.stderr}`);
+        return;
+      }
+      resolve(output.stdout);
+    }).catch(error => reject(`Failed to execute rsync: ${error}`));
+  });
+}
 
-  static Update(user, host, src, dest, executor) { // Update dest if src was updated
-    let userError = VALIDATE.IsStringInput(user);
-    if (userError)
-      return Promise.reject(`Failed to execute rsync: user is ${userError}`);
+function Update(user, host, src, dest, executor) { // Update dest if src was updated
+  let userError = VALIDATE.IsStringInput(user);
+  if (userError)
+    return Promise.reject(`Failed to execute rsync: user is ${userError}`);
 
-    let hostError = VALIDATE.IsStringInput(host);
-    if (hostError)
-      return Promise.reject(`Failed to execute rsync: host is ${hostError}`);
+  let hostError = VALIDATE.IsStringInput(host);
+  if (hostError)
+    return Promise.reject(`Failed to execute rsync: host is ${hostError}`);
 
-    let srcError = VALIDATE.IsStringInput(src);
-    if (srcError)
-      return Promise.reject(`Failed to execute rsync: source is ${srcError}`);
+  let srcError = VALIDATE.IsStringInput(src);
+  if (srcError)
+    return Promise.reject(`Failed to execute rsync: source is ${srcError}`);
 
-    let destError = VALIDATE.IsStringInput(dest);
-    if (destError)
-      return Promise.reject(`Failed to execute rsync: destination is ${destError}`);
+  let destError = VALIDATE.IsStringInput(dest);
+  if (destError)
+    return Promise.reject(`Failed to execute rsync: destination is ${destError}`);
 
-    if (!executor)
-      return Promise.reject(`Failed to execute rsync: Executor is required`);
+  if (!executor)
+    return Promise.reject(`Failed to execute rsync: Executor is required`);
 
-    return new Promise((resolve, reject) => {
-      executor.Execute('rsync', ['-a', '--update', src, `${user}@${host}:${dest}`]).then(output => {
-        if (output.stderr) {
-          reject(`Failed to execute rsync: ${output.stderr}`);
-          return;
-        }
-        resolve(output.stdout);
-      }).catch(error => reject(`Failed to execute rsync: ${error}`));
-    });
-  }
+  return new Promise((resolve, reject) => {
+    executor.Execute('rsync', ['-a', '--update', src, `${user}@${host}:${dest}`]).then(output => {
+      if (output.stderr) {
+        reject(`Failed to execute rsync: ${output.stderr}`);
+        return;
+      }
+      resolve(output.stdout);
+    }).catch(error => reject(`Failed to execute rsync: ${error}`));
+  });
+}
 
-  static Match(user, host, src, dest, executor) { // Copy files and then delete those NOT in src (Match dest to src)
-    let userError = VALIDATE.IsStringInput(user);
-    if (userError)
-      return Promise.reject(`Failed to execute rsync: user is ${userError}`);
+function Match(user, host, src, dest, executor) { // Copy files and then delete those NOT in src (Match dest to src)
+  let userError = VALIDATE.IsStringInput(user);
+  if (userError)
+    return Promise.reject(`Failed to execute rsync: user is ${userError}`);
 
-    let hostError = VALIDATE.IsStringInput(host);
-    if (hostError)
-      return Promise.reject(`Failed to execute rsync: host is ${hostError}`);
+  let hostError = VALIDATE.IsStringInput(host);
+  if (hostError)
+    return Promise.reject(`Failed to execute rsync: host is ${hostError}`);
 
-    let srcError = VALIDATE.IsStringInput(src);
-    if (srcError)
-      return Promise.reject(`Failed to execute rsync: source is ${srcError}`);
+  let srcError = VALIDATE.IsStringInput(src);
+  if (srcError)
+    return Promise.reject(`Failed to execute rsync: source is ${srcError}`);
 
-    let destError = VALIDATE.IsStringInput(dest);
-    if (destError)
-      return Promise.reject(`Failed to execute rsync: destination is ${destError}`);
+  let destError = VALIDATE.IsStringInput(dest);
+  if (destError)
+    return Promise.reject(`Failed to execute rsync: destination is ${destError}`);
 
-    if (!executor)
-      return Promise.reject(`Failed to execute rsync: Executor is required`);
+  if (!executor)
+    return Promise.reject(`Failed to execute rsync: Executor is required`);
 
-    return new Promise((resolve, reject) => {
-      executor.Execute('rsync', ['-a', '--delete-after', src, `${user}@${host}:${dest}`]).then(output => {
-        if (output.stderr) {
-          reject(`Failed to execute rsync: ${output.stderr}`);
-          return;
-        }
-        resolve(output.stdout);
-      }).catch(error => reject(`Failed to execute rsync: ${error}`));
-    });
-  }
+  return new Promise((resolve, reject) => {
+    executor.Execute('rsync', ['-a', '--delete-after', src, `${user}@${host}:${dest}`]).then(output => {
+      if (output.stderr) {
+        reject(`Failed to execute rsync: ${output.stderr}`);
+        return;
+      }
+      resolve(output.stdout);
+    }).catch(error => reject(`Failed to execute rsync: ${error}`));
+  });
+}
 
-  static DryRun(args, executor) { // Will execute without making changes (for testing command)
-    let argsError = argsValidator(args);
-    if (argsError)
-      return Promise.reject(`Failed to execute rsync: ${argsError}`);
+function DryRun(args, executor) { // Will execute without making changes (for testing command)
+  let argsError = argsValidator(args);
+  if (argsError)
+    return Promise.reject(`Failed to execute rsync: ${argsError}`);
 
-    if (!executor)
-      return Promise.reject(`Failed to execute rsync: Executor is required`);
+  if (!executor)
+    return Promise.reject(`Failed to execute rsync: Executor is required`);
 
-    return new Promise((resolve, reject) => {
-      executor.Execute('rsync', ['--dry-run'].concat(args)).then(output => {
-        if (output.stderr) {
-          reject(`Failed to execute rsync: ${output.stderr}`);
-          return;
-        }
-        resolve(output.stdout);
-      }).catch(error => reject(`Failed to execute rsync: ${error}`));
-    });
-  }
+  return new Promise((resolve, reject) => {
+    executor.Execute('rsync', ['--dry-run'].concat(args)).then(output => {
+      if (output.stderr) {
+        reject(`Failed to execute rsync: ${output.stderr}`);
+        return;
+      }
+      resolve(output.stdout);
+    }).catch(error => reject(`Failed to execute rsync: ${error}`));
+  });
+}
 
-  static Manual(args, executor) {  // args = [string | number]
-    let argsError = argsValidator(args);
-    if (argsError)
-      return Promise.reject(`Failed to execute rsync: ${argsError}`);
+function Manual(args, executor) {  // args = [string | number]
+  let argsError = argsValidator(args);
+  if (argsError)
+    return Promise.reject(`Failed to execute rsync: ${argsError}`);
 
-    if (!executor)
-      return Promise.reject(`Failed to execute rsync: Executor is required`);
+  if (!executor)
+    return Promise.reject(`Failed to execute rsync: Executor is required`);
 
-    return new Promise((resolve, reject) => {
-      executor.Execute('rsync', args).then(output => {
-        if (output.stderr) {
-          reject(`Failed to execute rsync: ${output.stderr}`);
-          return;
-        }
-        resolve(output.stdout);
-      }).catch(error => reject(`Failed to execute rsync: ${error}`));
-    });
-  }
+  return new Promise((resolve, reject) => {
+    executor.Execute('rsync', args).then(output => {
+      if (output.stderr) {
+        reject(`Failed to execute rsync: ${output.stderr}`);
+        return;
+      }
+      resolve(output.stdout);
+    }).catch(error => reject(`Failed to execute rsync: ${error}`));
+  });
 }
 
 //-----------------------------------
 // EXPORTS
 
 exports.Rsync = Rsync;
+exports.Update = Update;
+exports.Match = Match;
+exports.DryRun = DryRun;
+exports.Manual = Manual;
