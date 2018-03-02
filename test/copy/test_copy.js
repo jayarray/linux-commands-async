898438b11dfd9743f55dfe4682b6bd05463ecde1
Jesus Arevalo <isa90201@gmail.com>
2018-03-02 15:44:08 -0800

    Updated copy test

--- test/copy/test_copy.js
+++ test/copy/test_copy.js
@@ -1,31 +1,81 @@
 let EXPECT = require('chai').expect;
 
-let _path = require('path');
-let rootDir = _path.join(__dirname, '..', '..');
+let PATH = require('path');
+let rootDir = PATH.join(__dirname, '..', '..');
 
-let copyJs = _path.join(rootDir, 'copy.js');
-let COPY = require(copyJs).Copy;
+let copyJs = PATH.join(rootDir, 'copy.js');
+let COPY = require(copyJs);
+
+let fileJs = PATH.join(rootDir, 'file.js');
+let FILE = require(fileJs);
+
+let directoryJs = PATH.join(rootDir, 'directory.js');
+let DIRECTORY = require(directoryJs);
+
+let commandJs = PATH.join(rootDir, 'command.js');
+let COMMAND = require(commandJs);
 
 //------------------------------------------
 
 describe('*** copy.js ***', () => {
-  describe('Copy', () => {
-    describe('Copy(src, dest)', () => {
-      let src = copyJs;
-      let dest = _path.join(rootDir, 'delete_me.txt');
+  let executor = COMMAND.LOCAL;
 
-      it('Returns an error if src or dest are invalid.', () => {
-        COPY.Copy(null, dest).then(bool => EXPECT(false))
-          .catch(err => EXPECT(true));
-      });
+  describe('File(src, dest, executor)', () => {
+    let src = PATH.join(rootDir, 'README.md');
+    let dest = PATH.join(rootDir, 'delete_me.txt');
+    it('Returns an error if src is invalid.', () => {
+      COPY.File(null, dest, executor).then(success => EXPECT(false))
+        .catch(error => EXPECT(true));
+    });
 
-      it('Returns a boolean value if src and dest are valid.', () => {
-        COPY.Copy(src, dest).then(bool => {
-          FS.unlink(dest, (err) => {
-            EXPECT(true);
-          });
+    it('Returns an error if dest is invalid.', () => {
+      COPY.File(src, null, executor).then(success => EXPECT(false))
+        .catch(error => EXPECT(true));
+    });
+
+    it('Returns an error if executor is invalid.', () => {
+      COPY.File(src, dest, null).then(success => EXPECT(false))
+        .catch(error => EXPECT(true));
+    });
+
+    it('Actually copies a file.', () => {
+      COPY.File(src, dest, executor).then(success => {
+        FILE.Remove(dest, executor).then(success => {
+          EXPECT(true);
         }).catch(error => EXPECT(false));
-      });
+      }).catch(error => EXPECT(false));
+    });
+  });
+
+  describe('Directory(src, dest, executor)', () => {
+    let testDir = PATH.join(rootDir, 'delete_this_dir');
+    let testDirCopy = PATH.join(rootDir, 'dir_copy');
+
+    it('Returns an error if src is invalid.', () => {
+      COPY.Directory(null, testDirCopy, executor).then(success => EXPECT(false))
+        .catch(error => EXPECT(true));
+    });
+
+    it('Returns an error if dest is invalid.', () => {
+      COPY.Directory(testDir, null, executor).then(success => EXPECT(false))
+        .catch(error => EXPECT(true));
+    });
+
+    it('Returns an error if executor is invalid.', () => {
+      COPY.Directory(testDir, testDirCopy, null).then(success => EXPECT(false))
+        .catch(error => EXPECT(true));
+    });
+
+    it('Actually copies a directory.', () => {
+      DIRECTORY.Create(testDir, executor).then(success => {
+        COPY.Directory(testDir, testDirCopy, executor).then(success => {
+          DIRECTORY.Remove(testDir, executor).then(success => {
+            DIRECTORY.Remove(testDirCopy, executor).then(success => {
+              EXPECT(true);
+            }).catch(error => EXPECT(false));
+          }).catch(error => EXPECT(false));
+        }).catch(error => EXPECT(false));
+      }).catch(error => EXPECT(false));
     });
   });
 });
