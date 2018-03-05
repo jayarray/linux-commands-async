9802126062ab451554d9a661665a82549068b466
Jesus Arevalo <isa90201@gmail.com>
2018-03-05 15:12:16 -0800

    Created host.js for getting host info.

---
+++ host.js
@@ -0,0 +1,29 @@
+let VALIDATE = require('./validate.js');
+
+//---------------------------------------
+
+function CurrentHost(executor) {
+  if (!executor) {
+    return Promise.reject(`Failed to get host info: Executor is required`);
+  }
+
+  return new Promise((resolve, reject) => {
+    let cmdList = ['hostname', 'hostname -i'];
+
+    executor.Execute(cmdList.join('\n'), []).then(output => {
+      if (output.stderr) {
+        reject(`Failed to get host info: ${output.stderr}`);
+        return;
+      }
+
+      let lines = output.stdout.trim().split()
+        .filter(line => line && line != '' && line.trim() != '')
+        .map(line => line.trim());
+
+      resolve({
+        hostname: lines[0],
+        ipaddress: lines[1]
+      });
+    }).catch(error => `Failed to get host info: ${error}`);
+  });
+}
