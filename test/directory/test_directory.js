09b442d03375a2249bbbb313c73d22a634bc7ba9
Jesus Arevalo <isa90201@gmail.com>
2018-03-05 10:18:04 -0800

    Updated test for directory class.

--- test/directory/test_directory.js
+++ test/directory/test_directory.js
@@ -4,49 +4,67 @@ let PATH = require('path');
 let rootDir = PATH.join(__dirname, '..', '..');
 
 let directoryJs = PATH.join(rootDir, 'directory.js');
-let DIRECTORY = require(directoryJs).Directory;
+let DIRECTORY = require(directoryJs);
+
+let commandJs = PATH.join(rootDir, 'command.js');
+let COMMAND = require(commandJs);
 
 //------------------------------------------
 
 describe('*** directory.js ***', () => {
-  describe('Directory', () => {
-    let dirPath = PATH.join(rootDir, 'delete_this_test_dir');
+  let dirPath = PATH.join(rootDir, 'delete_this_test_dir');
+  let executor = COMMAND.LOCAL;
 
-    describe('Create(path)', () => {
-      it('Returns error if path is invalid.', () => {
-        DIRECTORY.Create(null).then(success => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Actually creates a directory.', () => {
-        DIRECTORY.Create(dirPath).then(success => EXPECT(true))
-          .catch(error => EXPECT(false));
-      });
+  describe('Create(path, executor)', () => {
+    it('Returns error if path is invalid.', () => {
+      DIRECTORY.Create(null, executor).then(success => EXPECT(false))
+        .catch(error => EXPECT(error).to.not.equal(null));
     });
 
-    describe('Size(path)', () => {
-      it('Returns error if path is invalid.', () => {
-        DIRECTORY.Size(null).then(size => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns integer if path is valid.', () => {
-        DIRECTORY.Size(dirPath).then(size => {
-          let isValid = Number.isInteger(size) && size >= 0;
-        }).catch(error => EXPECT(false));
-      });
+    it('Returns error if executor is invalid.', () => {
+      DIRECTORY.Create(dirPath, null).then(success => EXPECT(false))
+        .catch(error => EXPECT(error).to.not.equal(null));
     });
 
-    describe('Remove(path)', () => {
-      it('Returns error if path is invalid.', () => {
-        DIRECTORY.Remove(null).then(success => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
+    it('Actually creates a directory.', () => {
+      DIRECTORY.Create(dirPath, executor).then(success => EXPECT(true))
+        .catch(error => EXPECT(false));
+    });
+  });
 
-      it('Actually removes a directory.', () => {
-        DIRECTORY.Remove(dirPath).then(success => EXPECT(true))
-          .catch(error => EXPECT(false));
-      });
+  describe('Remove(path, executor)', () => {
+    it('Returns error if path is invalid.', () => {
+      DIRECTORY.Remove(null, executor).then(success => EXPECT(false))
+        .catch(error => EXPECT(error).to.not.equal(null));
+    });
+
+    it('Returns error if executor is invalid.', () => {
+      DIRECTORY.Remove(dirPath, null).then(success => EXPECT(false))
+        .catch(error => EXPECT(error).to.not.equal(null));
+    });
+
+    it('Actually removes a directory.', () => {
+      DIRECTORY.Remove(dirPath, executor).then(success => EXPECT(true))
+        .catch(error => EXPECT(false));
+    });
+  });
+
+  describe('Size(path, executor)', () => {
+    it('Returns error if path is invalid.', () => {
+      DIRECTORY.Size(null, executor).then(size => EXPECT(false))
+        .catch(error => EXPECT(error).to.not.equal(null));
+    });
+
+    it('Returns error if executor is invalid.', () => {
+      DIRECTORY.Size(rootDir, null).then(size => EXPECT(false))
+        .catch(error => EXPECT(error).to.not.equal(null));
+    });
+
+    it('Returns integer if path is valid.', () => {
+      DIRECTORY.Size(rootDir, executor).then(size => {
+        let isValid = Number.isInteger(size) && size >= 0;
+        EXPECT(isValid).to.equal(true);
+      }).catch(error => EXPECT(false));
     });
   });
 });
