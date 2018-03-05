1a60bf8357f8c51aba4ccd35b58cb8194b8fc3d6
Jesus Arevalo <isa90201@gmail.com>
2018-03-05 11:34:30 -0800

    Updated code for mkdir test.

--- test/mkdir/test_mkdir.js
+++ test/mkdir/test_mkdir.js
@@ -1,43 +1,40 @@
 let EXPECT = require('chai').expect;
 
-let _path = require('path');
-let rootDir = _path.join(__dirname, '..', '..');
+let PATH = require('path');
+let rootDir = PATH.join(__dirname, '..', '..');
 
-let mkdirJs = _path.join(rootDir, 'mkdir.js');
-let MKDIR = require(mkdirJs).Mkdir;
+let mkdirJs = PATH.join(rootDir, 'mkdir.js');
+let MKDIR = require(mkdirJs);
 
-let RIMRAF = require('rimraf');
+let directoryJs = PATH.join(rootDir, 'directory.js');
+let DIRECTORY = require(directoryJs);
+
+let commandJs = PATH.join(rootDir, 'command.js');
+let COMMAND = require(commandJs);
 
 //------------------------------------------
 
 describe('*** mkdir.js ***', () => {
-  describe('Mkdir', () => {
-    let validPath = _path.join(rootDir, 'delete_me');
+  let executor = COMMAND.LOCAL;
+  let testDirPath = PATH.join(rootDir, 'delete_me');
 
-    describe('Mkdir(path)', () => {
-      it('Returns an error if path is invalid.', () => {
-        MKDIR.Mkdir('').then(success => EXPECT(false))
-          .catch(error => EXPECT(true));
-      });
-
-      it('Actually creates a directory.', () => {
-        MKDIR.Mkdir(validPath).then(bool => {
-          RIMRAF(validPath, (err) => EXPECT(true));
-        }).catch(error => EXPECT(false));
-      });
+  describe('MakeDirectory(path, executor)', () => {
+    it('Returns an error if path is invalid.', () => {
+      MKDIR.MakeDirectory(null, executor).then(success => EXPECT(false))
+        .catch(error => EXPECT(true));
     });
 
-    describe('Mkdirp(path)', () => {
-      it('Returns an error if path is invalid.', () => {
-        MKDIR.Mkdirp(null).then(bool => EXPECT(false))
-          .catch(error => EXPECT(true));
-      });
+    it('Returns an error if executor is invalid.', () => {
+      MKDIR.MakeDirectory(testDirPath, null).then(success => EXPECT(false))
+        .catch(error => EXPECT(true));
+    });
 
-      it('Actually creates a directory.', () => {
-        MKDIR.Mkdirp(validPath).then(bool => {
-          RIMRAF(validPath, (err) => EXPECT(true));
+    it('Actually creates a directory.', () => {
+      MKDIR.MakeDirectory(testDirPath, executor).then(success => {
+        DIRECTORY.Remove(testDirPath, executor).then(success => {
+          EXPECT(true);
         }).catch(error => EXPECT(false));
-      });
+      }).catch(error => EXPECT(false));
     });
   });
 });
