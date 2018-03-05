e4e2b43db84a1ca85080dc7d96d44f4213266273
Jesus Arevalo <isa90201@gmail.com>
2018-03-05 12:42:59 -0800

    Uipdated code for move test.

--- test/move/test_move.js
+++ test/move/test_move.js
@@ -1,42 +1,52 @@
 let EXPECT = require('chai').expect;
 
-let _path = require('path');
-let rootDir = _path.join(__dirname, '..', '..');
+let PATH = require('path');
+let rootDir = PATH.join(__dirname, '..', '..');
 
-let moveJs = _path.join(rootDir, 'move.js');
-let MOVE = require(moveJs).Move;
+let moveJs = PATH.join(rootDir, 'move.js');
+let MOVE = require(moveJs);
 
-let FS = require('fs-extra');
+let fileJs = PATH.join(rootDir, 'file.js');
+let FILE = require(fileJs);
+
+let commandJs = PATH.join(rootDir, 'command.js');
+let COMMAND = require(commandJs);
 
 //------------------------------------------
 
 describe('*** move.js ***', () => {
-  describe('Move', () => {
-    describe('Move(path)', () => {
-      it('Returns an error if path is invalid.', () => {
-        MOVE.Move(undefined).then(success => EXPECT(false))
-          .catch(error => EXPECT(true));
-      });
+  let testFilepath = PATH.join(rootDir, 'delete_this_test_file.txt');
+  let testDest = PATH.join(rootDir, 'delete_me.txt');
+  let executor = COMMAND.LOCAL;
 
-      it('Actually moves a file.', () => {
-        // Write a test file
-        let src = _path.join(rootDir, 'delete_me.txt');
-        let text = 'Delete me!';
+  describe('Move(src, dest, executor)', () => {
+    it('Returns an error if src is invalid.', () => {
+      MOVE.Move(null, testDest, executor).then(success => EXPECT(false))
+        .catch(error => EXPECT(true));
+    });
 
-        FS.writeFile(src, text, (err) => {
-          if (err)
-            EXPECT(false);
+    it('Returns an error if dest is invalid.', () => {
+      MOVE.Move(testFilepath, null, executor).then(success => EXPECT(false))
+        .catch(error => EXPECT(true));
+    });
 
-          // Move it
-          let dest = _path.join(rootDir, 'delete_this_moved_file.txt');
-          MOVE.Move(src, dest).then(bool => {
-            // Delete it
-            FS.unlink(dest, (err) => {
-              EXPECT(true);
-            });
+    it('Returns an error if executor is invalid.', () => {
+      MOVE.Move(testFilepath, testDest, null).then(success => EXPECT(false))
+        .catch(error => EXPECT(true));
+    });
+
+    it('Actually moves a file.', () => {
+      // Write a test file
+      let src = PATH.join(rootDir, 'delete_me.txt');
+      let text = 'Delete me!';
+
+      FILE.Create(testFilepath, text, executor).then(success => {
+        MOVE.Move(testFilepath, testDest, executor).then(success => {
+          FILE.Remove(testDest, executor).then(success => {
+            EXPECT(true);
           }).catch(error => EXPECT(false));
-        });
-      });
+        }).catch(error => EXPECT(false));
+      }).catch(error => EXPECT(false));
     });
   });
 });
