30eff03ae3fa17115b5b16da679277c43dc46320
Jesus Arevalo <isa90201@gmail.com>
2018-03-05 16:10:34 -0800

    Updated code for rsync test.

--- test/rsync/test_rsync.js
+++ test/rsync/test_rsync.js
@@ -1,153 +1,127 @@
 let EXPECT = require('chai').expect;
 
-let _path = require('path');
-let rootDir = _path.join(__dirname, '..', '..');
+let PATH = require('path');
+let rootDir = PATH.join(__dirname, '..', '..');
 
-let rsyncJs = _path.join(rootDir, 'rsync.js');
-let RSYNC = require(rsyncJs).Rsync;
+let rsyncJs = PATH.join(rootDir, 'rsync.js');
+let RSYNC = require(rsyncJs);
 
-let FS = require('fs-extra');
+let commandJs = PATH.join(rootDir, 'command.js');
+let COMMAND = require(commandJs);
 
 //------------------------------------------
 
 describe('*** rsync.js ***', () => {
-  describe('Rsync', () => {
-    let user = 'user';
-    let host = 'host';
-    let src = '/path/to/src';
-    let dest = '/path/to/dest';
+  let executor = COMMAND.LOCAL;
+  let user = 'user';
+  let host = 'host';
+  let src = '/path/to/src';
+  let dest = '/path/to/dest';
 
-    let flags = ['a', 'v'];
-    let options = ['--size-only'];
+  let args = ['-a', `${user}@${host}:${dest}`];
 
-    describe('Rsync(user, host, src, dest)', () => {
-      it('Returns an error if user is invalid.', () => {
-        RSYNC.Rsync(null, host, src, dest).then(output => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns an error if host is invalid.', () => {
-        RSYNC.Rsync(user, undefined, src, dest).then(output => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns an error if src is invalid.', () => {
-        RSYNC.Rsync(user, host, '', dest).then(output => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns an error if dest is invalid.', () => {
-        RSYNC.Rsync(user, host, src, 3).then(output => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
+  describe('Rsync(user, host, src, dest, executor)', () => {
+    it('Returns an error if user is invalid.', () => {
+      RSYNC.Rsync(null, host, src, dest, executor).then(output => EXPECT(false))
+        .catch(error => EXPECT(error).to.not.equal(null));
     });
 
-    describe('Update(user, host, src, dest)', () => {
-      it('Returns an error if user is invalid.', () => {
-        RSYNC.Update(null, host, src, dest).then(output => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns an error if host is invalid.', () => {
-        RSYNC.Update(user, undefined, src, dest).then(output => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns an error if src is invalid.', () => {
-        RSYNC.Update(user, host, '', dest).then(output => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns an error if dest is invalid.', () => {
-        RSYNC.Update(user, host, src, 3).then(output => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
+    it('Returns an error if host is invalid.', () => {
+      RSYNC.Rsync(user, null, src, dest, executor).then(output => EXPECT(false))
+        .catch(error => EXPECT(error).to.not.equal(null));
     });
 
-    describe('Match(user, host, src, dest)', () => {
-      it('Returns an error if user is invalid.', () => {
-        RSYNC.Match(null, host, src, dest).then(output => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns an error if host is invalid.', () => {
-        RSYNC.Match(user, undefined, src, dest).then(output => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns an error if src is invalid.', () => {
-        RSYNC.Match(user, host, '', dest).then(output => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns an error if dest is invalid.', () => {
-        RSYNC.Match(user, host, src, 3).then(output => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
+    it('Returns an error if src is invalid.', () => {
+      RSYNC.Rsync(user, host, null, dest, executor).then(output => EXPECT(false))
+        .catch(error => EXPECT(error).to.not.equal(null));
     });
 
-    describe('Manual(user, host, src, dest, flags, options)', () => {
-      it('Returns an error if user is invalid.', () => {
-        RSYNC.Manual(null, host, src, dest, flags, options).then(output => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns an error if host is invalid.', () => {
-        RSYNC.Manual(user, undefined, src, dest, flags, options).then(output => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns an error if src is invalid.', () => {
-        RSYNC.Manual(user, host, '', dest, flags, options).then(output => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns an error if dest is invalid.', () => {
-        RSYNC.Manual(user, host, src, 3, flags, options).then(output => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns an error if flags is invalid.', () => {
-        RSYNC.Manual(user, host, src, dest, null, options).then(output => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
-
-      it('Returns an error if options is invalid.', () => {
-        RSYNC.Manual(user, host, src, dest, flags, null).then(output => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
+    it('Returns an error if dest is invalid.', () => {
+      RSYNC.Rsync(user, host, src, null, executor).then(output => EXPECT(false))
+        .catch(error => EXPECT(error).to.not.equal(null));
     });
 
-    describe('DryRun(user, host, src, dest)', () => {
-      it('Returns an error if user is invalid.', () => {
-        RSYNC.DryRun(null, host, src, dest, flags, options).then(output => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
+    it('Returns an error if executor is invalid.', () => {
+      RSYNC.Rsync(user, host, src, dest, null).then(output => EXPECT(false))
+        .catch(error => EXPECT(error).to.not.equal(null));
+    });
+  });
 
-      it('Returns an error if host is invalid.', () => {
-        RSYNC.DryRun(user, undefined, src, dest, flags, options).then(output => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
+  describe('Update(user, host, src, dest, executor)', () => {
+    it('Returns an error if user is invalid.', () => {
+      RSYNC.Update(null, host, src, dest, executor).then(output => EXPECT(false))
+        .catch(error => EXPECT(error).to.not.equal(null));
+    });
 
-      it('Returns an error if src is invalid.', () => {
-        RSYNC.DryRun(user, host, '', dest, flags, options).then(output => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
+    it('Returns an error if host is invalid.', () => {
+      RSYNC.Update(user, null, src, dest, executor).then(output => EXPECT(false))
+        .catch(error => EXPECT(error).to.not.equal(null));
+    });
 
-      it('Returns an error if dest is invalid.', () => {
-        RSYNC.DryRun(user, host, src, 3, flags, options).then(output => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
+    it('Returns an error if src is invalid.', () => {
+      RSYNC.Update(user, host, null, dest, executor).then(output => EXPECT(false))
+        .catch(error => EXPECT(error).to.not.equal(null));
+    });
 
-      it('Returns an error if flags is invalid.', () => {
-        RSYNC.DryRun(user, host, src, dest, null, options).then(output => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
+    it('Returns an error if dest is invalid.', () => {
+      RSYNC.Update(user, host, src, null, executor).then(output => EXPECT(false))
+        .catch(error => EXPECT(error).to.not.equal(null));
+    });
 
-      it('Returns an error if options is invalid.', () => {
-        RSYNC.DryRun(user, host, src, dest, flags, null).then(output => EXPECT(false))
-          .catch(error => EXPECT(error).to.not.equal(null));
-      });
+    it('Returns an error if executor is invalid.', () => {
+      RSYNC.Update(user, host, src, dest, null).then(output => EXPECT(false))
+        .catch(error => EXPECT(error).to.not.equal(null));
+    });
+  });
+
+  describe('Match(user, host, src, dest, executor)', () => {
+    it('Returns an error if user is invalid.', () => {
+      RSYNC.Match(null, host, src, dest, executor).then(output => EXPECT(false))
+        .catch(error => EXPECT(error).to.not.equal(null));
+    });
+
+    it('Returns an error if host is invalid.', () => {
+      RSYNC.Match(user, null, src, dest, executor).then(output => EXPECT(false))
+        .catch(error => EXPECT(error).to.not.equal(null));
+    });
+
+    it('Returns an error if src is invalid.', () => {
+      RSYNC.Match(user, host, null, dest, executor).then(output => EXPECT(false))
+        .catch(error => EXPECT(error).to.not.equal(null));
+    });
+
+    it('Returns an error if dest is invalid.', () => {
+      RSYNC.Match(user, host, src, null, executor).then(output => EXPECT(false))
+        .catch(error => EXPECT(error).to.not.equal(null));
+    });
+
+    it('Returns an error if executor is invalid.', () => {
+      RSYNC.Match(user, host, src, dest, null).then(output => EXPECT(false))
+        .catch(error => EXPECT(error).to.not.equal(null));
+    });
+  });
+
+  describe('Manual(args, executor)', () => {
+    it('Returns an error if args is invalid.', () => {
+      RSYNC.Manual(null, executor).then(output => EXPECT(false))
+        .catch(error => EXPECT(error).to.not.equal(null));
+    });
+
+    it('Returns an error if executor is invalid.', () => {
+      RSYNC.Manual(args, null).then(output => EXPECT(false))
+        .catch(error => EXPECT(error).to.not.equal(null));
+    });
+  });
+
+  describe('DryRun(args, executor)', () => {
+    it('Returns an error if args is invalid.', () => {
+      RSYNC.DryRun(null, executor).then(output => EXPECT(false))
+        .catch(error => EXPECT(error).to.not.equal(null));
+    });
+
+    it('Returns an error if executor is invalid.', () => {
+      RSYNC.DryRun(args, null).then(output => EXPECT(false))
+        .catch(error => EXPECT(error).to.not.equal(null));
     });
   });
 });
