56e526637bfac77c9e2f8388f76110e57eceec8b
Jesus Arevalo <isa90201@gmail.com>
2018-03-09 11:01:24 -0800

    Fixed typo.

--- chown.js
+++ chown.js
@@ -53,7 +53,7 @@ function ArgsValidator(args) {
 /**
  * Change user ownership.
  * @param {Array<string>} paths A list of paths that will change user ownership.
- * @param {string|Number} newOwnerId Username or user ID number.
+ * @param {string|number} newOwnerId Username or user ID number.
  * @param {boolean} isRecursive Assign as true if ownership is to be applied recursively, otherwise assign as false.
  * @param {Command} executor Command object that will execute the command.
  * @returns {Promise} Returns a promise that will resolve if successful, otherwise it returns an error.
@@ -90,7 +90,7 @@ function ChangeOwner(paths, newOwnerId, isRecursive, executor) { // newOwner can
 /**
  * Change group ownership.
  * @param {Array<string>} paths A list of paths that will change group ownership.
- * @param {string|Number} newGroupId Group name or group ID number.
+ * @param {string|number} newGroupId Group name or group ID number.
  * @param {boolean} isRecursive Assign as true if ownership is to be applied recursively, otherwise assign as false.
  * @param {Command} executor Command object that will execute the command.
  * @returns {Promise} Returns a promise that will resolve if successful, otherwise it returns an error.
@@ -127,8 +127,8 @@ function ChangeGroup(paths, newGroupId, isRecursive, executor) { // newOwner can
 /**
  * Change user and group ownership simultaneously.
  * @param {Array<string>} paths A list of paths that will change user and group ownership.
- * @param {string|Number} newOwnerId Username or user ID number.
- * @param {string|Number} newGroupId Group name or group ID number.
+ * @param {string|number} newOwnerId Username or user ID number.
+ * @param {string|number} newGroupId Group name or group ID number.
  * @param {boolean} isRecursive Assign as true if ownership is to be applied recursively, otherwise assign as false.
  * @param {Command} executor Command object that will execute the command.
  * @returns {Promise} Returns a promise that will resolve if successful, otherwise it returns an error.
@@ -168,7 +168,7 @@ function ChangeOwnerAndGroup(paths, newOwnerId, newGroupId, isRecursive, executo
 
 /**
  * Change ownership.
- * @param {Array<string|Number>} args A list of args used in 'chown' command.
+ * @param {Array<string|number>} args A list of args used in 'chown' command.
  * @param {Command} executor Command object that will execute the command.
  * @returns {Promise} Returns a promise that will resolve if successful, otherwise it returns an error.
  */
